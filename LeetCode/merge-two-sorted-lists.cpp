56e6e6d3fa544fca3934e1c023fc0faa018d51f2
bluesilence <bluesilencemail@126.com>
2014-11-29 15:45:11 +0800

    Create merge-two-sorted-lists.cpp
    Pay attention to the dummy head and move p forward.

---
+++ LeetCode/merge-two-sorted-lists.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        if (!l1)
+            return l2;
+        
+        if (!l2)
+            return l1;
+            
+        ListNode *p1 = l1, *p2 = l2;
+        
+        //Dummy head node
+        ListNode *head = new ListNode(0);
+        ListNode *p = head;
+        while (p1 && p2) {
+            if (p1->val < p2->val) {
+                p->next = p1;
+                p1 = p1->next;
+            } else {
+                p->next = p2;
+                p2 = p2->next;
+            }
+            
+            p = p->next;
+        }
+        
+        if (p1)
+            p->next = p1;
+        else if (p2)
+            p->next = p2;
+            
+        p = head->next;
+        delete head;
+        
+        return p;
+    }
+};

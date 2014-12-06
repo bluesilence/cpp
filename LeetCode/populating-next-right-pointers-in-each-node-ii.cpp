a26cd744b661a4b192f5ec4928c1e0cd38669f15
bluesilence <bluesilencemail@126.com>
2014-12-06 09:29:31 +0800

    Create populating-next-right-pointers-in-each-node-ii.cpp
    Copy the code from populating-next-right-pointers-in-each-node.cpp

---
+++ LeetCode/populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+// Exactly the same as version i
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if (!root)
+            return;
+            
+        //Level traverse
+        queue<TreeLinkNode*> q1, q2;
+        q1.push(root);
+        
+        TreeLinkNode* pre = NULL, *curr = NULL;
+        while (!q1.empty() || !q2.empty()) {
+            pre = NULL;
+            if (q1.empty()) {
+                while (!q2.empty()) {
+                    curr = q2.front();
+                    if (curr->left)
+                        q1.push(curr->left);
+                    
+                    if (curr->right)
+                        q1.push(curr->right);
+                    
+                    q2.pop();
+                    if (pre) {
+                        pre->next = curr;
+                    }
+                
+                    pre = curr;
+                }
+            } else {
+                 while (!q1.empty()) {
+                    curr = q1.front();
+                    if (curr->left)
+                        q2.push(curr->left);
+                    
+                    if (curr->right)
+                        q2.push(curr->right);
+                    
+                    q1.pop();
+                    if (pre) {
+                        pre->next = curr;
+                    }
+                
+                    pre = curr;
+                }
+            }
+                
+            if (pre)
+                pre->next = NULL;
+        }
+    }
+};

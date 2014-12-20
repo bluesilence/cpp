ecbf168050cf96f352ca0ee3688958393b003452
bluesilence <bluesilencemail@126.com>
2014-12-20 09:30:59 +0800

    Update longest-substring-without-repeating-characters.cpp
    Change idx from starting Index - 1 to starting Index

--- LeetCode/longest-substring-without-repeating-characters.cpp
+++ LeetCode/longest-substring-without-repeating-characters.cpp
@@ -3,8 +3,8 @@ public:
     int lengthOfLongestSubstring(string s) {
         int max = 0;  
           
-        // The (start index - 1) of a sub string 
-        int idx = -1;  
+        // The start index of a sub string 
+        int idx = 0;  
           
         // The latest occurrence of each char 
         int locs[256];  
@@ -12,14 +12,14 @@ public:
         
         for (int i = 0; i < s.size(); i++) {  
             // If s[i] already appeared in current sub string
-            if (locs[s[i]] > idx) {  
+            if (locs[s[i]] >= idx) {  
                 // Set the start of next sub string to be locs[s[i]] + 1
-                idx = locs[s[i]];  
+                idx = locs[s[i]] + 1;  
             }
             
             // Update max length of sub strings 
-            if (i - idx > max) {  
-                max = i - idx;  
+            if (i - idx + 1 > max) {  
+                max = i - idx + 1;  
             }
             
             // Update the latest occurrence of current char

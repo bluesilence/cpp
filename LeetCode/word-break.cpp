39ad965634f7a87ccc430baea34184397215e873
bluesilence <bluesilencemail@126.com>
2014-12-28 12:11:40 +0800

    Create word-break.cpp
    Another difficult DP question...Need to understand how to record the information of sub questions.

---
+++ LeetCode/word-break.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool wordBreak(string s, unordered_set<string> &dict) {
+        //Record if a substring from 0 to i can fit in the dict
+        vector<bool> wordB(s.length() + 1, false);
+        
+        wordB[0] = true;
+        for (int i = 1; i < s.length() + 1; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (wordB[j] && dict.find(s.substr(j, i - j)) != dict.end()) {
+                    wordB[i] = true;
+                    break;
+                }
+            }
+        }
+        return wordB[s.length()];
+    }
+};

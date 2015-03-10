efb2b222813f3cb54880b02eefb5b1a1e91c7e77
bluesilence <bluesilencemail@126.com>
2015-03-10 20:23:28 +0800

    Update longest-ascending-sequence.cpp
    Add comments as study of DP

--- DP/longest-ascending-sequence.cpp
+++ DP/longest-ascending-sequence.cpp
@@ -4,45 +4,47 @@ using namespace std;
 
 class Solution {
 public:
-	int LongestAscendingSequence(vector<int> &nums) {
-		if (nums.empty())
-			return 0;
+    int LongestAscendingSequence(vector<int> &nums) {
+        if (nums.empty())
+            return 0;
 
-		int result = 1;
-		int n = nums.size();
-		int *longestTrail = new int[n];
-		//memset will fill byte by byte, so 1 won't produce correct result below:
-		//memset(longestTrail, 1, sizeof(longestTrail));
-		for (int i = 0; i < n; i++)
-			longestTrail[i] = 1;
+        int result = 1;
+        int n = nums.size();
 
-		for (int i = 1; i < n; i++) {
-			int maxTrail = longestTrail[i];
-			for (int j = 0; j < i; j++) {
-				if (nums[i] > nums[j] && longestTrail[j] + 1 > maxTrail)
-					maxTrail = longestTrail[j] + 1;
-			}
+        // longestTrail[i] stands for the length of longest ascending subsequence that ends with nums[i]
+        int *longestTrail = new int[n];
+        //memset will fill byte by byte, so 1 won't produce correct result below:
+        //memset(longestTrail, 1, sizeof(longestTrail));
+        for (int i = 0; i < n; i++)
+            longestTrail[i] = 1;
 
-			longestTrail[i] = maxTrail;
-			if (maxTrail > result)
-				result = maxTrail;
-		}
+        for (int i = 1; i < n; i++) {
+            int tempTrail = longestTrail[i];
 
-		delete longestTrail;
+            // Check all the candidate trails
+            // longestTrail[i] = max(longestTrail[i], longestTrail[j] where (0 <= j < i && nums[j] < nums[i]) + 1)
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && longestTrail[j] + 1 > tempTrail)
+                    tempTrail = longestTrail[j] + 1;
+            }
 
-		return result;
-	}
+            longestTrail[i] = tempTrail;
+            if (tempTrail > result)
+                result = tempTrail;
+        }
+
+        delete longestTrail;
+
+        return result;
+    }
 };
 
 void main() {
-	Solution test;
-	vector<int> inputs;
-	inputs.push_back(3);
-	inputs.push_back(2);
-	inputs.push_back(4);
-	inputs.push_back(1);
-	inputs.push_back(5);
-	inputs.push_back(4);
-	inputs.push_back(6);
-	cout<<test.LongestAscendingSequence(inputs);
+    Solution test;
+    vector<int> inputs;
+    inputs.push_back(3);
+    inputs.push_back(4);
+    inputs.push_back(1);
+    inputs.push_back(2);
+    cout<<test.LongestAscendingSequence(inputs);
 }

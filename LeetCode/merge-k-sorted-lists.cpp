019876c0fded22b53a84e57b349415d931b63a2d
bluesilence <bluesilencemail@126.com>
2015-03-25 16:41:30 +0800

    Update merge-k-sorted-lists.cpp
    Another solution: Use c++ priority queue.
    
    //Min Heap: if a->val > b->val, then a < b
    struct cmp {
        bool operator() (ListNode *a, ListNode *b) {
            return a->val > b->val;
        }
    };
    
    class Solution {
    public:
        ListNode *mergeKLists(vector<ListNode *> &lists) {
            priority_queue<ListNode *, vector<ListNode *>, cmp> queue;
            
            for (int i = 0; i < lists.size(); i++) {
                if (lists[i] != NULL)
                    queue.push(lists[i]);
            }
            
            ListNode *dummyHead = new ListNode(0), *p = dummyHead;
            ListNode *tmp;
            while (!queue.empty()) {
                tmp = queue.top();
                queue.pop();
                p->next = tmp;
                p = p->next;
                if (tmp->next) {
                    queue.push(tmp->next);
                }
            }
            
            ListNode *head = dummyHead->next;
            delete dummyHead;
            
            return head;
        }
    };

--- LeetCode/merge-k-sorted-lists.cpp
+++ LeetCode/merge-k-sorted-lists.cpp
@@ -1,35 +1,55 @@
-//Min Heap: if a->val > b->val, then a < b
-struct cmp {
-    bool operator() (ListNode *a, ListNode *b) {
-        return a->val > b->val;
-    }
-};
-
 class Solution {
 public:
     ListNode *mergeKLists(vector<ListNode *> &lists) {
-        priority_queue<ListNode *, vector<ListNode *>, cmp> queue;
+        int k = lists.size();
+        if (k == 0)
+            return NULL;
         
-        for (int i = 0; i < lists.size(); i++) {
-            if (lists[i] != NULL)
-                queue.push(lists[i]);
-        }
+        int step;
         
-        ListNode *dummyHead = new ListNode(0), *p = dummyHead;
-        ListNode *tmp;
-        while (!queue.empty()) {
-            tmp = queue.top();
-            queue.pop();
-            p->next = tmp;
-            p = p->next;
-            if (tmp->next) {
-                queue.push(tmp->next);
+        //Divide-and-Conquer -- Time complexity: O(nklogk), where n is the average length of all the lists
+        while (k > 1) {
+            step = (k + 1) / 2;
+            for (int i = 0; i < k/2; i++) {
+                lists[i] = merge2Lists(lists[i], lists[i + step]);
             }
+            
+            k = step;
         }
         
-        ListNode *head = dummyHead->next;
+        return lists[0];
+    }
+
+private:
+    ListNode *merge2Lists(ListNode *list1, ListNode *list2) {
+        if (!list1)
+            return list2;
+        
+        if (!list2)
+            return list1;
+        
+        ListNode *dummyHead = new ListNode(0);
+        ListNode *p = dummyHead;
+        while (list1 && list2) {
+            if (list1->val <= list2->val) {
+                p->next = list1;
+                list1 = list1->next;
+            } else {
+                p->next = list2;
+                list2 = list2->next;
+            }
+            
+            p = p->next;
+        }
+        
+        if (list1)
+            p->next = list1;
+        else if (list2)
+            p->next = list2;
+        
+        p = dummyHead->next;
         delete dummyHead;
         
-        return head;
+        return p;
     }
 };

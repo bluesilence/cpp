a5a93dfb138fab8c905fc52f1de41b87700406ad
bluesilence <bluesilencemail@126.com>
2015-03-25 14:21:07 +0800

    Update jump-game.cpp

--- LeetCode/jump-game.cpp
+++ LeetCode/jump-game.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     bool canJump(int A[], int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
         if (n == 0)
             return false;
             
         int step = A[0];
         
-        for(int i = 1; i < n; i++)
-            if (step > 0)
-            {
+        //Greedy strategy: Always maintain the max steps at each index
+        for (int i = 1; i < n; i++) {
+            if (step > 0) {
                 step--;
                 step = max(step, A[i]);
             }
-            else
+            else {
                 return false;
+            }
+        }
                 
         return true;
     }

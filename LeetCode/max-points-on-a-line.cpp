320b0c54316447a9f42844454d1a3903ce75be1f
bluesilence <bluesilencemail@126.com>
2015-04-21 23:02:07 +0800

    Create max-points-on-a-line.cpp
    Detailed explanation:
    http://www.cnblogs.com/x1957/p/3491872.html

---
+++ LeetCode/max-points-on-a-line.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maxPoints(vector<Point> &points) {
+        if (points.size() == 0)
+            return 0;
+            
+        unordered_map<double, int> count;
+        int size = points.size();
+        int ans = 0;
+        
+        for (int i = 0; i < size; i++) {
+            count.clear();
+            int x = points[i].x;
+            int y = points[i].y;
+            int dups = 0;
+            
+            for(int j = 0; j < size; j++) {
+                if(i != j) {
+                    int x1 = points[j].x;
+                    int y1 = points[j].y;
+                    if(x == x1 && y == y1) {
+                        dups++;
+                    } else if (x == x1) {   //Vertical line
+                        count[(double)INT_MAX]++;
+                    } else {
+                        double k = (double)(y1-y) / (double)(x1-x); //Note: convert int to double
+                        count[k]++;
+                    }
+                }
+            }
+            
+            for (auto it = count.begin(); it != count.end(); it++){
+                ans = max(ans, it->second + dups);
+            }
+            
+            ans = max(ans, dups);   //all the points are the same
+        }
+        
+        return ans + 1; //Add the original point that was enumerated in the outer loop
+    }
+};

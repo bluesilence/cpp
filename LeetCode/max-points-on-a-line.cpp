aefb4f01fce9832131d95d55314ad7017e06cccb
bluesilence <bluesilencemail@126.com>
2015-04-22 07:32:22 +0800

    Update max-points-on-a-line.cpp
    The inner loop only needs to start from i+1

--- LeetCode/max-points-on-a-line.cpp
+++ LeetCode/max-points-on-a-line.cpp
@@ -14,18 +14,16 @@ public:
             int y = points[i].y;
             int dups = 0;
             
-            for(int j = 0; j < size; j++) {
-                if(i != j) {
-                    int x1 = points[j].x;
-                    int y1 = points[j].y;
-                    if(x == x1 && y == y1) {
-                        dups++;
-                    } else if (x == x1) {   //Vertical line
-                        count[(double)INT_MAX]++;
-                    } else {
-                        double k = (double)(y1-y) / (double)(x1-x); //Note: convert int to double
-                        count[k]++;
-                    }
+            for(int j = i+1; j < size; j++) {
+                int x1 = points[j].x;
+                int y1 = points[j].y;
+                if(x == x1 && y == y1) {
+                    dups++;
+                } else if (x == x1) {   //Vertical line
+                    count[(double)INT_MAX]++;
+                } else {
+                    double k = (double)(y1-y) / (double)(x1-x); //Note: convert int to double
+                    count[k]++;
                 }
             }
             
@@ -33,7 +31,7 @@ public:
                 ans = max(ans, it->second + dups);
             }
             
-            ans = max(ans, dups);   //all the points are the same
+            ans = max(ans, dups);   //Only duplicate points
         }
         
         return ans + 1; //Add the original point that was enumerated in the outer loop

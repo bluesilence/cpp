8f1cbc5430e8cd2261db80d08d29ef46dba38ca1
bluesilence <bluesilencemail@126.com>
2015-05-04 08:54:00 +0800

    Update string-to-integer-atoi.cpp
    Second time.
    Bugs:
    1. The order of *10 and +digit;
    2. When to test boundary conditions.

--- LeetCode/string-to-integer-atoi.cpp
+++ LeetCode/string-to-integer-atoi.cpp
@@ -1,29 +1,49 @@
 class Solution {
 public:
-    int atoi(const char *str) {
-        while (*str == ' ')
-            str++;
+    int myAtoi(string str) {
+        long long result = 0;
+        
+        if (str.empty())
+            return result;
+        
+        int i = 0;
+        // Remove spaces
+        while (i < str.size() && str[i] == ' ')
+            i++;
+        
+        if (i == str.size())
+            return result;
+        
+        // Recognize sign
+        bool isNegative = false;
+        if (str[i] == '+' || str[i] == '-') {
+            if (str[i] == '-')
+                isNegative = true;
             
-        int pos = 1;
-        if (*str == '+' || *str == '-') {
-            if (*str == '-')
-                pos = -1;
-            str++;
+            i++;
         }
         
-        double result = 0;
-        while (*str != '\0' && *str >= '0' && *str <= '9') {
-            result = result * 10 + *str - '0';
-            str++;
+        // Parse number
+        while (i < str.size()) {
+            int digit = str[i] - '0';
+            if (digit >= 0 && digit <= 9) {
+                result *= 10;
+                result += digit;
+                // Added test case requires boundary conditions must be determined here, because a too large positive long long may result in negative number after negating, and the returning value will be min int instead of max int
+                if (!isNegative && result >= numeric_limits<int>::max())
+                    return numeric_limits<int>::max();
+                else if (isNegative && (0 - result) <= numeric_limits<int>::min())
+                    return numeric_limits<int>::min();
+            } else {
+                break;
+            }
+            
+            i++;
         }
         
-        result = result * pos;
-        
-        if (result > numeric_limits<int>::max())
-            return numeric_limits<int>::max();
-        else if (result < numeric_limits<int>::min())
-            return numeric_limits<int>::min();
-        else
-            return (int)result;
+        if (isNegative)
+            result = 0 - result;
+            
+        return result;
     }
 };

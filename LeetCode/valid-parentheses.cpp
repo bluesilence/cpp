0895bbc108bd20058cb7db25a4583a57e358b50c
bluesilence <bluesilencemail@126.com>
2015-05-05 22:12:53 +0800

    Update valid-parentheses.cpp
    Use switch is clearer than if-else.
    Remember to add "break" at the end of each case...

--- LeetCode/valid-parentheses.cpp
+++ LeetCode/valid-parentheses.cpp
@@ -1,55 +1,58 @@
 class Solution {
 public:
     bool isValid(string s) {
-        //Use an array to store the count of parantheses
-        //Scan the string, for each character, if it is right parantheses, check if the corresponding left parantheses' count > right's count. If not, return false
-        //0: '('
-        //1: '{'
-        //2: '['
-        //3: ')'
-        //4: '}'
-        //5: ']'
-        int parenCounts[6] = {0};
+        //For right parentheses, the other 2 kind of left parentheses must be all matched
+        //And the left parentheses of the same kind must have some left not matched yet
+        //Otherwise, the string is invalid
+        vector<int> counts(6, 0);
         stack<char> lastParen;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s[i];
-            
-            if ('(' == ch) {
-                parenCounts[0]++;
-                lastParen.push('(');
-            } else if ('{' == ch) {
-                parenCounts[1]++;
-                lastParen.push('{');
-            } else if('[' == ch) {
-                parenCounts[2]++;
-                lastParen.push('[');
-            } else if (')' == ch) {   //Right parentheses must appear after left one
-                if ((parenCounts[0] > parenCounts[3]) && lastParen.top() == '(') {
-                    parenCounts[3]++;
-                    lastParen.pop();
-                } else {
-                    return false;
-                }
-            } else if ('}' == ch) {
-                if (parenCounts[1] > parenCounts[4] && lastParen.top() == '{') {
-                    parenCounts[4]++;
-                    lastParen.pop();
-                } else {
-                    return false;
-                }
-            } else if (']' == ch) {
-                if (parenCounts[2] > parenCounts[5] && lastParen.top() == '[') {
-                    parenCounts[5]++;
-                    lastParen.pop();
-                } else {
-                    return false;
-                }
+        
+        for (int i = 0; i < s.size(); i++) {
+            switch (s[i]) {
+                case '(':
+                    counts[0]++;
+                    lastParen.push('(');
+                    break;
+                case '[':
+                    counts[1]++;
+                    lastParen.push('[');
+                    break;
+                case '{':
+                    counts[2]++;
+                    lastParen.push('{');
+                    break;
+                case ')':   //Bug: && is prior to >, need to use ()
+                    if ((counts[0] > counts[3]) && lastParen.top() == '(') {
+                        counts[3]++;
+                        lastParen.pop();
+                    } else {
+                        return false;
+                    }
+                    
+                    break;
+                case ']':
+                    if ((counts[1] > counts[4]) && lastParen.top() == '[') {
+                        counts[4]++;
+                        lastParen.pop();
+                    } else {
+                        return false;
+                    }
+                    
+                    break;
+                case '}':
+                    if ((counts[2] > counts[5]) && lastParen.top() == '{') {
+                        counts[5]++;
+                        lastParen.pop();
+                    } else {
+                        return false;
+                    }
+                    
+                    break;
             }
         }
         
-        //Each left parentheses must be closed by a right one
         for (int i = 0; i < 3; i++) {
-            if (parenCounts[i] != parenCounts[i + 3])
+            if (counts[i] != counts[i+3])
                 return false;
         }
         

725384607936f4c92d1ba3fb1791b464c8250651
bluesilence <bluesilencemail@126.com>
2015-05-07 07:27:43 +0800

    Update climbing-stairs.cpp
    Use vector instead of int*

--- LeetCode/climbing-stairs.cpp
+++ LeetCode/climbing-stairs.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int *methods = new int[n + 1];
-        methods[0] = 1;
-        methods[1] = 1;
-        
-        for (int i = 2; i <= n; i++) {
-            methods[i] = methods[i-1] + methods[i-2];
+        vector<int> ways(n+1, 0);
+        ways[1] = 1;
+        ways[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            ways[i] = ways[i-1] + ways[i-2];
         }
         
-        int result = methods[n];
-        delete methods;
-        
-        return result;
+        return ways[n];
     }
 };

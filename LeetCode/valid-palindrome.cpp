fe0e68eb9e2a52a09dc540d8952179c2f5cd7f97
bluesilence <bluesilencemail@126.com>
2015-05-07 08:29:11 +0800

    Update valid-palindrome.cpp
    Second time.

--- LeetCode/valid-palindrome.cpp
+++ LeetCode/valid-palindrome.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int begin = 0, end = s.size() - 1;
+        if (s.size() < 1)
+            return true;
         
-        while (begin < end) {
-            while (begin < end && !isValidChar(s[begin]))
-                begin++;
-            while (begin < end && !isValidChar(s[end]))
+        int start = 0;
+        int end = s.size() - 1;
+        
+        while (start < end) {
+            while (start < end && !isAlphaNumeric(s[start]))
+                start++;
+            
+            while (start < end && !isAlphaNumeric(s[end]))
                 end--;
-            if (toLower(s[begin]) == toLower(s[end])) {
-                begin++;
-                end--;
-            } else {
+                
+            if (toLower(s[start]) != toLower(s[end]))
                 return false;
-            }
+            
+            start++;
+            end--;
         }
         
         return true;
     }
-    
+
 private:
-    bool isValidChar(char c) {
-        return c >= 'A' && c <= 'Z' ||  c >= 'a' && c <= 'z' || c >= '0' && c <= '9';
+    bool isAlphaNumeric(char c) {
+        return (c >= 'a' && c <= 'z')
+            || (c >= 'A' && c <= 'Z')
+            || (c >= '0' && c <= '9');
     }
     
     char toLower(char c) {
-        if (c <= 'Z' && c >= 'A')
-            return c + 'a' - 'A';
-        else return c;
+        if (c >= 'A' && c <= 'Z')
+            return c - 'A' + 'a';
+        else
+            return c;
     }
 };

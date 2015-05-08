6dfca743be93f2ab1b6fd6c613a810e2f24ff090
bluesilence <bluesilencemail@126.com>
2015-05-08 08:17:26 +0800

    Update roman-to-integer.cpp
    Second time:
    Performance improvement:
    1. Scan in 1 loop;
    2. Use method instead of dict to map roman digit.

--- LeetCode/roman-to-integer.cpp
+++ LeetCode/roman-to-integer.cpp
@@ -2,26 +2,45 @@ class Solution {
 public:
     //羅馬數字共有7個，即I（1）、V（5）、X（10）、L（50）、C（100）、D（500）和M（1000）
     int romanToInt(string s) {
-        map<char, int> dict;
-        dict['I'] = 1;
-        dict['V'] = 5;
-        dict['X'] = 10;
-        dict['L'] = 50;
-        dict['C'] = 100;
-        dict['D'] = 500;
-        dict['M'] = 1000;
-        
         int result = 0;
-        for (int i = 0; i + 1 < s.size(); i++) {
-            if (dict[s[i]] >= dict[s[i+1]])
-                result += dict[s[i]];
+        
+        int pre = getInt(s[0]);
+        int curr;
+        
+        result += pre;
+        
+        //There could only be 1 smaller digit at the left of the larger digit
+        //So just compare current and previous digit
+        for (int i = 1; i < s.size(); i++) {
+            curr = getInt(s[i]);
+            if (pre >= curr)
+                result += curr;
             else
-                result -= dict[s[i]];
+                result = result - 2 * pre + curr;
+            
+            pre = curr;
         }
         
-        //The last digit must be added, not subtracted
-        result += dict[s[s.size()-1]];
-        
         return result;
+    }
+
+private:
+    int getInt(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+        }
     }
 };

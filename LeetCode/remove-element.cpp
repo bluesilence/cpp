6bf1532f7f0acfebe8f60aaf1113c32473a42e29
bluesilence <bluesilencemail@126.com>
2015-05-08 22:37:33 +0800

    Update remove-element.cpp
    Second time.
    Algorithm is similar to quick sort, use right side as valid numbers, exchange invalid number in the left side to the number in the right side.

--- LeetCode/remove-element.cpp
+++ LeetCode/remove-element.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    int removeElement(int A[], int n, int elem) {
-        if (!A || n < 1)
-            return 0;
+    int removeElement(vector<int>& nums, int val) {
+        int length = nums.size();
+        
+        int start = 0;
+        int end = length - 1;
+        
+        while (start <= end) {
+            while (end >= start && nums[end] == val)
+                end--;
             
-        int fast = 0, slow = 0;
-        
-        //Find the first occurrence of elem
-        while (slow < n && A[slow] != elem) {
-            slow++;
-        }
-        
-        if (slow < n) {
-            fast = slow;
-        } else {
-            return n;
-        }
-
-        //Copy non-elem numbers onto elems
-        while (fast < n) {
-            while (fast < n && A[fast] == elem) {
-                fast++;
-            }
-        
-            while (fast < n && A[fast] != elem) {
-                A[slow++] = A[fast++];
+            if (end >= start) {
+                //The '=' is necessary for case: ([2], 3)
+                while (start <= end && nums[start] != val)
+                    start++;
+                    
+                if (start < end && nums[start] == val) {
+                    nums[start] = nums[end];
+                    end--;
+                    start++;
+                }
             }
         }
         
-        return slow;
+        return ++end;
     }
 };

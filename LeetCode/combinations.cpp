49eb0d8f2ff70768211264a704379b7f52ed1e23
bluesilence <bluesilencemail@126.com>
2015-05-09 10:10:51 +0800

    Update combinations.cpp
    Second time.
    Basic recursion of permutation.

--- LeetCode/combinations.cpp
+++ LeetCode/combinations.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    vector<vector<int> > combine(int n, int k) {
-        vector<vector<int> > results;
-        vector<int> combination;
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> results;
+        if (n < 1 || k < 1 || k > n)
+            return results;
         
-        combineCore(n, k, results, combination, 0, 1);
+        vector<int> result;
+        int i = 1;
+        combineCore(results, result, n, i, k);
+        
         return results;
     }
-    
+
 private:
-    void combineCore(int n, int k, vector<vector<int> > &results, vector<int> &combination, int index, int start) {
-        if (index == k) {
-            results.push_back(combination);
-        } else {
-            for (int i = start; i <= n; i++) {
-                combination.push_back(i);
-                combineCore(n, k, results, combination, index + 1, i + 1);
-                combination.pop_back();
-            }
+    void combineCore(vector<vector<int>> &results, vector<int> &result, int n, int i, int count) {
+        if (n - i + 1 < count)  //Remaining numbers are not enough
+            return;
+        
+        if (count == 0) {
+            results.push_back(result);
+            return;
         }
+        
+        result.push_back(i);
+        combineCore(results, result, n, i + 1, count - 1);
+        result.pop_back();
+        combineCore(results, result, n, i + 1, count);
     }
+    
 };

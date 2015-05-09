fd0a2f607d0dd19e7c83cd5dbecb8aa0a752170c
bluesilence <bluesilencemail@126.com>
2015-05-09 10:52:59 +0800

    Update subsets.cpp
    Second time.
    The input integers are distinct, so no need to dedup after sorting S in order.

--- LeetCode/subsets.cpp
+++ LeetCode/subsets.cpp
@@ -2,28 +2,25 @@ class Solution {
 public:
     vector<vector<int> > subsets(vector<int> &S) {
         vector<vector<int> > results;
-        
         if (S.empty())
             return results;
         
         vector<int> result;
-        
-        // The answer enforce an order, so need to sort S first
         sort(S.begin(), S.end());
-        selectSubset(S, results, result, 0);
+        subsetsCore(results, result, S, 0);
         
         return results;
     }
 
 private:
-    void selectSubset(vector<int> &S, vector<vector<int> >& results, vector<int> &result, int index) {
-        if (index == S.size()) {
+    void subsetsCore(vector<vector<int> > &results, vector<int> &result, vector<int> &S, int begin) {
+        if (begin == S.size()) {
             results.push_back(result);
         } else {
-            selectSubset(S, results, result, index + 1);
-            result.push_back(S[index]);
-            selectSubset(S, results, result, index + 1);
+            result.push_back(S[begin]);
+            subsetsCore(results, result, S, begin+1);
             result.pop_back();
+            subsetsCore(results, result, S, begin+1);
         }
     }
 };

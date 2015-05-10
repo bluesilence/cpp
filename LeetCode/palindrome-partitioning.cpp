844536b91f82c79a041bc1e7f1b05508ae28d43c
bluesilence <bluesilencemail@126.com>
2015-05-10 11:07:13 +0800

    Update palindrome-partitioning.cpp
    Second time.
    Move the termination before calling next recursion can improve performance slightly.

--- LeetCode/palindrome-partitioning.cpp
+++ LeetCode/palindrome-partitioning.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-        //Use recursion to generate all combinations of palindrome
+        //DFS. Check if s[i...j] is palindrome. If so, collect substring s(i, j) and recursively check from s[j+1]
+        //When reaching the end of s, collect this set of substrings
         vector<vector<string>> results;
         if (s.empty())
             return results;
         
-        vector<string> combination;
-        partitionCore(s, results, combination, 0);
+        vector<string> result;
+        partitionCore(results, result, s, 0);
         
         return results;
     }
 
 private:
-    void partitionCore(string s, vector<vector<string>> &results, vector<string> &combination, int n) {
-        if (n >= s.size()) {
-            results.push_back(combination);
-            return;
-        }
-        
-		for (int j = n; j < s.size(); j++) {
-			if (isPalindrome(s, n, j)) {
-				string p = s.substr(n, j - n + 1);
-				combination.push_back(p);
-				partitionCore(s, results, combination, j + 1);
-				combination.pop_back();
-			}
+    void partitionCore(vector<vector<string>> &results, vector<string> &result, string &s, int begin) {
+        for (int end = begin; end < s.size(); end++) {
+            if (isPalindrome(s, begin, end)) {
+                result.push_back(s.substr(begin, end - begin + 1));
+                if (end + 1 < s.size()) {
+                    partitionCore(results, result, s, end + 1);
+                } else {
+                    results.push_back(result);
+                }
+                    
+                result.pop_back();
+            }
         }
     }
     
-    bool isPalindrome(string s, int start, int end) {
-        if (s.empty() || start > end || start < 0)
-            return false;
-        
-        while (start < end) {
-            if (s[start++] != s[end--])
+    bool isPalindrome(string s, int begin, int end) {
+        if (begin < 0 || end >= s.size())
+            throw "Invalid index of s";
+            
+        while (begin < end) {
+            if (s[begin++] != s[end--])
                 return false;
         }
         

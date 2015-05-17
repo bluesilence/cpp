c97e6431d98c53c1d52fabf31b950d17882fd7aa
bluesilence <bluesilencemail@126.com>
2015-05-17 09:25:49 +0800

    Update reverse-integer.cpp
    Second time.

--- LeetCode/reverse-integer.cpp
+++ LeetCode/reverse-integer.cpp
@@ -1,43 +1,31 @@
-#include <limits>;
-#include <iostream>;
-using namespace std;
-
 class Solution {
 public:
     int reverse(int x) {
-        if (0 == x)
+        //If using x, the min negative int may overflow when converted to positive int. So convert x to long long first.
+        long long input = x;
+        long long result;
+        if (input == 0)
+            result = 0;
+        else if (input > 0)
+            result = reverseCore(input);
+        else
+            result = -1 * reverseCore(0 - input);
+        
+        if (x > 0 && result > numeric_limits<int>::max() || x < 0 && result < numeric_limits<int>::min())
             return 0;
-        
-        long long absX = (x < 0) ? (0 - x) : x;
-        long long rawResult = reverseCore(absX);
-        
-        if (x < 0) {
-            if (rawResult - 1 <= numeric_limits<int>::max())
-                return (int)(0 - rawResult);
-            else
-                return 0;
-        }
-        else if (x > 0) {
-            if (rawResult <= numeric_limits<int>::max())
-                return (int)rawResult;
-            else
-                return 0;
-        }
+        else
+            return (int)result;
     }
-    
+
 private:
-    long long reverseCore(long long x) {
+    long long reverseCore(long long input) {
         long long result = 0;
-        while(x > 0) {
-            result = result * 10 + x % 10;
-            x /= 10;
+        while (input > 0) {
+            result *= 10;
+            result += input % 10;
+            input /= 10;
         }
         
         return result;
     }
 };
-
-void main() {
-	Solution test;
-	cout<<test.reverse(-123);
-}

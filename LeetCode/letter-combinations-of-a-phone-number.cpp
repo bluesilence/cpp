8c0d3bf90cd2c6e5fb30634846b90975af1a1634
bluesilence <bluesilencemail@126.com>
2015-05-17 10:21:22 +0800

    Update letter-combinations-of-a-phone-number.cpp
    Second time.
    string also supports push_back and pop_back.

--- LeetCode/letter-combinations-of-a-phone-number.cpp
+++ LeetCode/letter-combinations-of-a-phone-number.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
+        string dict[10] = { " ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        
         vector<string> results;
-        combine(digits, results, "", 0);
+        if (!digits.empty()) {
+            string result;
+            combinationCore(results, result, dict, digits, 0);
+        }
         
         return results;
     }
-    
-    Solution() {
-        mappings['0'] = " ";
-        mappings['1'] = "";
-        mappings['2'] = "abc";
-        mappings['3'] = "def";
-        mappings['4'] = "ghi";
-        mappings['5'] = "jkl";
-        mappings['6'] = "mno";
-        mappings['7'] = "pqrs";
-        mappings['8'] = "tuv";
-        mappings['9'] = "wxyz";
-    }
 
 private:
-    map<char, string> mappings;
-    
-    void combine(const string &digits, vector<string> &results, string result, int i) {
-        if (i >= digits.size()) {
+    void combinationCore(vector<string> &results, string &result, string *dict, string digits, int index) {
+        if (index == digits.size()) {
             results.push_back(result);
-            return;
-        }
-        
-        string characters = mappings[digits[i]];
-        for (int j = 0; j < characters.size(); j++) {
-            combine(digits, results, result + characters[j], i + 1);
+        } else {
+            string letters = dict[digits[index] - '0'];
+            for (int i = 0; i < letters.size(); i++) {
+                result.push_back(letters[i]);
+                combinationCore(results, result, dict, digits, index + 1);
+                result.pop_back();
+            }
         }
     }
 };

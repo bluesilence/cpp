f989aec8bfdd560c9896ac96a5644d7a594efa42
bluesilence <bluesilencemail@126.com>
2015-05-21 07:14:59 +0800

    Update search-in-rotated-sorted-array.cpp
    Second time.
    Use vector instead of int array.

--- LeetCode/search-in-rotated-sorted-array.cpp
+++ LeetCode/search-in-rotated-sorted-array.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    int search(int A[], int n, int target) {
-        if (!A || n < 1)
+    int search(vector<int>& nums, int target) {
+        if (nums.size() < 1)
             return -1;
-           
-        int front = 0, end = n-1;
+            
+        int start = 0, end = nums.size() - 1;
         int mid;
-        while (front <= end) {
-            mid = (front + end) / 2;
-            if (target == A[mid]) {
+        
+        //First determine an ordered sub array using mid, start and end, then check where target is located.
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (target == nums[mid]) {
                 return mid;
-            } else if (A[front] <= A[mid]) {
-                if (target >= A[front] && target < A[mid])  //target exists in A[front, mid-1]
+            } else if (nums[start] <= nums[mid]) {  //mid is in the left part
+                if (target >= nums[start] && target < nums[mid]) {  //target is in [start, mid)
                     end = mid - 1;
-                else   //target exists in A[mid+1, end]
-                    front = mid + 1;
-            } else {    //A[front] > A[mid]
-                if (target > A[mid] && target <= A[end])  //target exists in A[mid+1, end]
-                    front = mid + 1;
-                else    //target exists in A[front, mid-1]
+                } else {    //target is in (mid, end]
+                    start = mid + 1;
+                }
+            } else {    //mid is in the right part
+                if (target > nums[mid] && target <= nums[end]) {   //target is in (mid, end]
+                    start = mid + 1;
+                } else {    //target is in [start, mid)
                     end = mid - 1;
+                }
             }
         }
         

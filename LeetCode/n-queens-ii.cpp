4f8bc5f78ffe96eb02d8cef0a1046e9f8f2fca0f
bluesilence <bluesilencemail@126.com>
2015-05-24 10:57:54 +0800

    Update n-queens-ii.cpp
    Second time.
    To check validity, check all the columns before current column.
    If isValid and current column is the last column, this is 1 valid solution.

--- LeetCode/n-queens-ii.cpp
+++ LeetCode/n-queens-ii.cpp
@@ -4,32 +4,35 @@ public:
         if (n < 1)
             return 0;
         
-        vector<int> q(n, -1);
+        vector<int> rowForQueens(n, -1);
         
-        return countQueens(q, n);
+        return countQueens(rowForQueens, n);
     }
     
-    int countQueens(vector<int> &q, int n, int index = 0) {
-        if (index == n)
-            return 1;
-        
+private:
+    int countQueens(vector<int> &rowForQueens, const int n, int col = 0) {
         int sum = 0;
         for (int i = 0; i < n; i++) {
-            if (isLegal(q, index, i)) { //Can put a Queen at row index, col i
-                q[index] = i;
-                sum += countQueens(q, n, index+1);
+            if (isValid(rowForQueens, col, i)) {
+                rowForQueens[col] = i;
+                if (col + 1 < n)
+                    sum += countQueens(rowForQueens, n, col + 1);
+                else
+                    sum += 1;
             }
         }
         
         return sum;
     }
     
-private:
-    bool isLegal(vector<int> &q, int n, int index) {
-        for (int i = 0; i < n; i++)
-            if (q[i] == index || q[i] - i == index - n || q[i] + i == index + n)
+    bool isValid(vector<int> &rowForQueens, const int col, const int row) {
+        for (int i = 0; i < col; i++) {
+            if (row == rowForQueens[i] ||
+                rowForQueens[i] - row == i - col ||
+                rowForQueens[i] - row == col - i)
                 return false;
+        }
         
-      return true;
+        return true;
     }
 };

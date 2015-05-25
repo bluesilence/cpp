dfd02d7493572c04ec5b3e4160a7192aba8ac677
bluesilence <bluesilencemail@126.com>
2015-05-25 08:42:55 +0800

    Update minimum-path-sum.cpp
    Second time.
    1. Greedy strategy doesn't work since it may miss some optimal paths in global.
    2. Manually initialize sums matrix is faster than using "= grid".

--- LeetCode/minimum-path-sum.cpp
+++ LeetCode/minimum-path-sum.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int> > &grid) {
-        if (grid.size() == 0 || grid[0].size() == 0)
+    int minPathSum(vector<vector<int>>& grid) {
+        if (grid.empty() || grid[0].empty())
             return 0;
+            
+        const int m = grid.size();
+        const int n = grid[0].size();
         
-        int r = grid.size(), c = grid[0].size();
+        vector<vector<int> > sums(m, vector<int>(n));
         
-        //Initialization
-        //the 0th row and 0th col are boundaries with value 0
-        int **mins = new int*[r + 1];
-        for (int i = 0; i <= r; i++) {
-            mins[i] = new int[c + 1];
-			memset(mins[i], 0, sizeof(int) * (c + 1));
-        }
-        
-        for (int i = 1; i <= r; i++) {
-            for (int j = 1; j <= c; j++) {
-                mins[i][j] += grid[i-1][j-1];
-                
-                if (i > 1 && j > 1 || i == 1 && j == 1)
-                    mins[i][j] += min(mins[i-1][j], mins[i][j-1]);
-                else if (1 == i && j > 1)
-                    mins[i][j] += mins[i][j-1]; //Can only add from left
-                else if (1 == j && i > 1)
-                    mins[i][j] += mins[i-1][j]; //Can only add from top
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                sums[i][j] = grid[i][j];
             }
         }
         
-        int sum = mins[r][c];
-        
-        //Clean-up
-        for (int i = 0; i <= r; i++) {
-            delete mins[i];
+        for (int i = m-1; i >= 0; i--) {
+            for (int j = n-1; j >= 0; j--) {
+                if (i < m-1 && j < n-1) {
+                    sums[i][j] += min(sums[i+1][j], sums[i][j+1]);
+                } else if (i < m-1) {
+                    sums[i][j] += sums[i+1][j];
+                } else if (j < n-1) {
+                    sums[i][j] += sums[i][j+1];
+                }
+            }
         }
         
-        delete mins;
-        
-        return sum;
+        return sums[0][0];
     }
 };

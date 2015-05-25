478a315158034c103d4a8844badf7cd2045d6b34
bluesilence <bluesilencemail@126.com>
2015-05-25 08:22:29 +0800

    Update unique-paths-ii.cpp
    Second time.
    For the last column and last row, 1 obstacle will block the entire path.

--- LeetCode/unique-paths-ii.cpp
+++ LeetCode/unique-paths-ii.cpp
@@ -4,20 +4,36 @@ public:
         const int m = obstacleGrid.size();
         const int n = obstacleGrid[0].size();
         
-        vector<vector<int> > f(m, vector<int>(n));
-               
-        f[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+        if (m < 1 || n < 1 || obstacleGrid[m-1][n-1] == 1)
+            return 0;
         
-        for (int i = 1; i < m; i++)
-            f[i][0] = obstacleGrid[i][0] == 1 ? 0 : f[i-1][0];
-            
-        for (int i = 1; i < n; i++)
-            f[0][i] = obstacleGrid[0][i] == 1 ? 0 : f[0][i-1];
-            
-        for (int i = 1; i < m; i++)
-            for(int j = 1; j < n; j++)
-                f[i][j] = obstacleGrid[i][j] == 1 ? 0 : f[i-1][j] + f[i][j-1];
-                
-        return f[m-1][n-1];
+        vector<vector<int> > pathCounts(m, vector<int>(n, 0));
+        
+        pathCounts[m-1][n-1] = 1;
+        
+        for (int i = m-2; i >= 0; i--) {    //Last column
+            if (obstacleGrid[i][n-1] == 0)
+                pathCounts[i][n-1] = 1;
+            else
+                break;  //All the following grids are blocked
+        }
+        
+        for (int i = n-2; i >= 0; i--) {    //Last row
+            if (obstacleGrid[m-1][i] == 0)
+                pathCounts[m-1][i] = 1;
+            else
+                break;  //All the following grids are blocked
+        }
+        
+        for (int i = m-2; i >= 0; i--) {
+            for (int j = n-2; j >= 0; j--) {
+                if (obstacleGrid[i][j] == 0)
+                    pathCounts[i][j] = pathCounts[i+1][j] + pathCounts[i][j+1];
+                else
+                    pathCounts[i][j] = 0;
+            }
+        }
+        
+        return pathCounts[0][0];
     }
 };

3edf2851d62dd29262f69d18fc5b07b83336c6c4
bluesilence <bluesilencemail@126.com>
2015-05-26 07:27:01 +0800

    Update edit-distance.cpp
    Second time.
    Use recursion will cause TLE. So use DP.

--- LeetCode/edit-distance.cpp
+++ LeetCode/edit-distance.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        //The 0th row/col is boundary
-        int row = word1.length() + 1;
-        int col = word2.length() + 1;
+        if (word1.size() > word2.size())    //Keep word1 the shorter one can avoid duplicate code
+            return minDistance(word2, word1);
+            
+        if (word1.empty())
+            return word2.size();
         
-        vector<vector<int> > f(row, vector<int>(col));
-
-        //Initialize boundaries
-        for (int i = 0; i < row; i++)
-            f[i][0] = i;
-
-        for (int i = 0; i < col; i++)
-            f[0][i] = i;
-
-        for (int i = 1; i < row; i++)
-            for (int j = 1; j < col; j++){
-                if (word1[i - 1] == word2[j - 1])
-                    f[i][j] = f[i - 1][j - 1];
-                else    //Replace
-                    f[i][j] = f[i - 1][j - 1] + 1;
-                
-                //Find min in Replace, Delete and Insert
-                f[i][j] = min(f[i][j], min(f[i - 1][j] + 1, f[i][j - 1] + 1));
+        const int m = word1.size() + 1;
+        const int n = word2.size() + 1;
+        
+        //dp[i][j] stands for the min Distance between word1[0...i-1] and word2[0...j-1]
+        vector<vector<int> > dp(m, vector<int>(n, 0));
+        
+        for (int i = 0; i < am; i++) {
+            dp[i][0] = i;
+        }
+        
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = j;
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (word1[i-1] == word2[j-1]) { //Caution! The index of dp starts from 1, while the index of string starts from 0
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]);
+                }
             }
-
-        return f[row - 1][col - 1];
+        }
+        
+        return dp[m-1][n-1];
     }
 };

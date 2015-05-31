de167d0ffc5801ccd3d155b69c892a06b2f93947
bluesilence <bluesilencemail@126.com>
2015-06-01 07:48:00 +0800

    Update longest-substring-without-repeating-characters.cpp
    Second time.
    Using set is very slow. Use bool array instead.

--- LeetCode/longest-substring-without-repeating-characters.cpp
+++ LeetCode/longest-substring-without-repeating-characters.cpp
@@ -1,31 +1,32 @@
-class Solution {  
-public:  
+class Solution {
+public:
     int lengthOfLongestSubstring(string s) {
-        int max = 0;  
-          
-        // The start index of a sub string 
-        int idx = 0;  
-          
-        // The latest occurrence of each char 
-        int locs[256];  
-        memset(locs, -1, sizeof(int) * 256);  
+        //Use 2 pointers to constrain the range of substring without repeating letters
+        //Use a set to record the distinct letters of the substring
+        if (s.empty())
+            return 0;
         
-        for (int i = 0; i < s.size(); i++) {  
-            // If s[i] already appeared in current sub string
-            if (locs[s[i]] >= idx) {  
-                // Set the start of next sub string to be locs[s[i]] + 1
-                idx = locs[s[i]] + 1;  
+        int begin = 0;
+        int end = 0;
+        
+        bool occurredChar[256] = {  false };
+        
+        int maxLen = 0;
+        int tmpLen = 0;
+        while (end < s.size()) {
+            if (occurredChar[s[end]] == false) {  //Move the window to right
+                occurredChar[s[end]] = true;
+                end++;
+                tmpLen++;
+                
+                maxLen = maxLen > tmpLen ? maxLen : tmpLen;
+            } else {    //Shrink the window from left
+                occurredChar[s[begin]] = false;
+                begin++;
+                tmpLen--;
             }
-            
-            // Update max length of sub strings 
-            if (i - idx + 1 > max) {  
-                max = i - idx + 1;  
-            }
-            
-            // Update the latest occurrence of current char
-            locs[s[i]] = i;  
         }
         
-        return max;  
-    }  
-};  
+        return maxLen;
+    }
+};

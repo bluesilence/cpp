a1e9e84539e2a66b0dfa87bed3993cc972a273a7
bluesilence <bluesilencemail@126.com>
2015-06-01 08:19:19 +0800

    Update longest-palindromic-substring.cpp
    Second time.
    Consider each character in s as the centre of the palindrome, then calculate the length of odd-length palindrome and even-length palindrome.

--- LeetCode/longest-palindromic-substring.cpp
+++ LeetCode/longest-palindromic-substring.cpp
@@ -1,48 +1,66 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int len = s.size();
-        int longestLen = 1;
-        int tempLen = 1;
-        int start = 0;
-        int end = 0;
-        for (int i = 0; i < len; i++) {
-            maxPalindromeLen(s, len, i, tempLen, start, end);
+        if (s.size() < 2)
+            return s;
+        
+        int tmpLen = 0;
+        int maxLen = 0;
+        int tmpStart = 0;
+        int tmpEnd = 0;
+        int maxStart = 0;
+        int maxEnd = 0;
+        
+        for (int i = 0; i + 1 < s.size(); i++) {
+            longestPalindrome(s, i, tmpStart, tmpEnd);
+            tmpLen = tmpEnd - tmpStart + 1;
+            if (tmpLen > maxLen) {
+                maxLen = tmpLen;
+                maxStart = tmpStart;
+                maxEnd = tmpEnd;
+            }
         }
         
-        return s.substr(start, end - start + 1);
+        return s.substr(maxStart, maxEnd - maxStart + 1);
     }
 
 private:
-    //Find the longest palindrome centered from each character of the string
-    int maxPalindromeLen(string s, int len, int index, int &tempMax, int &start, int &end) {
-        if (len < 2)
-            return len;
+    void longestPalindrome(const string &s, int centre, int &start, int &end) {
+        if (centre < 0 || centre >= s.size())
+            return;
         
-        int oddStart = index - 1, oddEnd = index + 1;
-        while (oddStart >= 0 && oddEnd < len && s[oddStart] == s[oddEnd]) {
-            oddStart--;
-            oddEnd++;
+        int evenStart = 0;
+        int evenEnd = 0;
+        //Even length palindrome
+        if (s[centre] == s[centre + 1]) {
+            evenStart = centre;
+            evenEnd = centre + 1;
+            while (evenStart > 0 && evenEnd + 1 < s.size() && s[evenStart - 1] == s[evenEnd + 1]) {
+                evenStart--;
+                evenEnd++;
+            }
         }
         
-        int maxLenOdd = oddEnd - oddStart - 1;
-        
-        int evenStart = index - 1, evenEnd = index;
-        while (evenStart >= 0 && evenEnd < len && s[evenStart] == s[evenEnd]) {
-            evenStart--;
-            evenEnd++;
+        int oddStart = 0;
+        int oddEnd = 0;
+        //Odd length palindrome
+        if (centre > 0 && s[centre - 1] == s[centre + 1]) {
+            oddStart = centre - 1;
+            oddEnd = centre + 1;
+            while (oddStart > 0 && oddEnd + 1 < s.size() && s[oddStart - 1] == s[oddEnd + 1]) {
+                oddStart--;
+                oddEnd++;
+            }
         }
         
-        int maxLenEven = evenEnd - evenStart - 1;
-        
-        if (maxLenOdd > maxLenEven && maxLenOdd > tempMax) {
-            start = oddStart + 1;
-            end = oddEnd - 1;
-            tempMax = maxLenOdd;
-        } else if (maxLenEven > maxLenOdd && maxLenEven > tempMax) {
-            start = evenStart + 1;
-            end = evenEnd - 1;
-            tempMax = maxLenEven;
+        int evenLen = evenEnd - evenStart + 1;
+        int oddLen = oddEnd - oddStart + 1;
+        if (evenLen > oddLen) {
+            start = evenStart;
+            end = evenEnd;
+        } else {
+            start = oddStart;
+            end = oddEnd;
         }
     }
 };

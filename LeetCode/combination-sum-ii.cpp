7e59d7923919eddb4f8a0940372ef49f1f096968
bluesilence <bluesilencemail@126.com>
2015-06-07 13:58:38 +0800

    Update combination-sum-ii.cpp
    Second time.
    Note:
    1. Pruning: L22
    2. Deduping: L30
    3. L34~39

--- LeetCode/combination-sum-ii.cpp
+++ LeetCode/combination-sum-ii.cpp
@@ -1,42 +1,62 @@
 class Solution {
 public:
-    vector<vector<int> > combinationSum2(vector<int> &candidates, int target) {
-        vector<vector<int> > results;
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> results;
         
         if (candidates.empty())
             return results;
         
         vector<int> result;
-        //Combination's numbers must appear in ascending order
+        
+        //The requirement is that the elements in a combination must be in non-descending order
         sort(candidates.begin(), candidates.end());
-        combinationCore(candidates, results, result, target, 0);
+        
+        combinationCore(results, result, candidates, target, 0);
         
         return results;
     }
 
 private:
-    void combinationCore(vector<int> &candidates, vector<vector<int> > &results, vector<int> &result, int target, int index) {
+    void combinationCore(vector<vector<int>> &results, vector<int> &result, vector<int> &candidates, int target, int index) {
         int i = index;
-        while (i < candidates.size() && candidates[i] <= target) {
+        while (i < candidates.size() && candidates[i] <= target) {    //Since candidates are sorted, target must be >= candidates[i] to form a combination
+            //Select candidates[i]
             result.push_back(candidates[i]);
-            if (target == candidates[i]) {   //Found 1 combination
+            
+            if (candidates[i] == target) {
                 results.push_back(result);
                 result.pop_back();
-                return; //Stop further recursion to avoid duplicate combinations
+                
+                return; //Found 1 candidate == target, skip the following checks since the combinations will be duplicate
             } else {
-                combinationCore(candidates, results, result, target - candidates[i], i+1); //Use candidate i
+                combinationCore(results, result, candidates, target - candidates[i], i + 1);
                 result.pop_back();
+                //Below is not needed, because L37 has moved i to next distinct number, and L33 has poped up candidates[i]
+                //combinationCore(results, result, candidates, target, index + 1);
                 
                 i = moveToNextDistinctNumber(candidates, i);
+                if (i < 0)  //No more distinct numbers
+                    break;
             }
         }
     }
     
     int moveToNextDistinctNumber(vector<int> &candidates, int i) {
-        int j = i+1;
-        while (j < candidates.size() && candidates[i] == candidates[j])
-            j++;
-                
-        return j;
+        if (i < 0) {
+            return 0;
+        } else if (i >= candidates.size()) {
+            return -1;
+        } else {
+            int pre = candidates[i];
+            int j = i + 1;
+            while (j < candidates.size() && candidates[j] == pre) {
+                j++;
+            }
+            
+            if (j < candidates.size())
+                return j;
+            else
+                return -1;
+        }
     }
 };

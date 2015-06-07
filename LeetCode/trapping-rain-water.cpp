1dd43147f5512f0ee2ebe7eef939e6d3418486fd
bluesilence <bluesilencemail@126.com>
2015-06-08 07:52:22 +0800

    Update trapping-rain-water.cpp
    Second time.
    Note:
    1. L14, L19: initialization.

--- LeetCode/trapping-rain-water.cpp
+++ LeetCode/trapping-rain-water.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    // For each individual slot, the capacity depends on the leftHighest and rightHighest
-    // The naive solution will be O(N*N)
-    // We can use DP to optimize, and get the list leftHighest and rightHighest in O(N)
-    // The overall time complexity can be reduced to O(N)
-    int trap(int A[], int n) {
-        vector<int> leftHighest, rightHighest;
+    int trap(vector<int>& height) {
+        //The height of water trapped at each x unit is determined by min(leftSummit, rightSummit) - currentElevation
+        //Left/Right summit is the highest elevation from the left/right of the map to currentElevation
+        const int n = height.size();
         
-        int highest=0;
-        for(int i = 0;i < n;i++){
-            leftHighest.push_back(highest);
-            highest = max(highest, A[i]);
+        if (n < 1)
+            return 0;
+            
+        vector<int> leftSummit(n, 0);
+        vector<int> rightSummit(n, 0);
+        
+        leftSummit[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftSummit[i] = max(leftSummit[i-1], height[i]);
         }
         
-        highest=0;
-        for(int i = n - 1;i >= 0;i--){
-            rightHighest.push_back(highest);
-            highest = max(highest, A[i]);
+        rightSummit[n-1] = height[n-1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightSummit[i] = max(rightSummit[i+1], height[i]);
         }
         
-        reverse(rightHighest.begin(), rightHighest.end());
-        
-        int water = 0;
-        for(int i=0;i<n;i++){
-            water += max(0, min(leftHighest[i], rightHighest[i]) - A[i]);
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += max(0, min(leftSummit[i], rightSummit[i]) - height[i]);
         }
         
-        return water;
+        return sum;
     }
 };

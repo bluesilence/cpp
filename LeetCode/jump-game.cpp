7a82ae349e278fd883521758ac4b84dff5c60364
bluesilence <bluesilencemail@126.com>
2015-06-09 22:36:21 +0800

    Update jump-game.cpp
    Second time.

--- LeetCode/jump-game.cpp
+++ LeetCode/jump-game.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    bool canJump(int A[], int n) {
-        if (n == 0)
-            return false;
-            
-        int step = A[0];
+    bool canJump(vector<int>& nums) {
+        int maxReachableIndex = 0;
         
-        //Greedy strategy: Always maintain the max steps at each index
-        for (int i = 1; i < n; i++) {
-            if (step > 0) {
-                step--;
-                step = max(step, A[i]);
-            }
-            else {
+        for (int i = 0; i < nums.size(); i++) {
+            maxReachableIndex = max(maxReachableIndex, i + nums[i]);
+            
+            if (maxReachableIndex >= nums.size() - 1) {
+                return true;
+            } else if (maxReachableIndex <= i) {
                 return false;
             }
         }
-                
-        return true;
+        
+        return false;
     }
 };

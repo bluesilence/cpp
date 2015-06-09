e13e79cad5c9a95c52d45b929b489e95184420ec
bluesilence <bluesilencemail@126.com>
2015-06-09 21:14:37 +0800

    Update rotate-image.cpp
    Second time.
    Added variables for clarity.

--- LeetCode/rotate-image.cpp
+++ LeetCode/rotate-image.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
-    void rotate(vector<vector<int> > &matrix) {
+    void rotate(vector<vector<int>>& matrix) {
+        if (matrix.empty() || matrix[0].empty())
+            return;
+        
+        if (matrix.size() != matrix[0].size())
+            throw "The height and width of the matrix must be the same.";
+        
         const int n = matrix.size();
-        for (int i = 0; i < n/2; i++) { //Rotate from outer circle to inner circle
-            for (int j = i; j < n-1-i; j++) {   //Replace 4 elements of 4 directions at one time
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[n-1-j][i];
-                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
-                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
-                matrix[j][n-1-i] = tmp;
+        
+        const int round = n / 2;
+        
+        for (int i = 0; i < round; i++) {
+            int begin = i;
+            int end = n - 1 - i;
+            
+            for (int j = begin; j < end; j++) {
+                int topLeft = matrix[i][j];
+                int topRight = matrix[j][end];
+                int bottomRight = matrix[end][n-1-j];
+                int bottomLeft = matrix[n-1-j][begin];
+                
+                matrix[j][end] = topLeft;
+                matrix[end][n-1-j] = topRight;
+                matrix[n-1-j][begin] = bottomRight;
+                matrix[i][j] = bottomLeft;
             }
         }
     }

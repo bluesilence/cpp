4c18ccc63d9003368cd3fbf8aed0b6136b644623
bluesilence <bluesilencemail@126.com>
2015-06-10 08:49:58 +0800

    Update plus-one.cpp
    Second time.
    Note:
    The order of lines in L14~16.

--- LeetCode/plus-one.cpp
+++ LeetCode/plus-one.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    vector<int> plusOne(vector<int> &digits) {
-        if (digits.size() < 1)
+    vector<int> plusOne(vector<int>& digits) {
+        if (digits.empty())
             return digits;
             
-        //1 extra digit for carry from the most significant digit
-        vector<int> result(digits.size() + 1, 0);
+        reverse(digits.begin(), digits.end());
+        const int n = digits.size();
         
-        for (int i = 1; i < result.size(); i++)
-            result[i] = digits[i-1];
+        digits[0]++;
         
-        int carry = 1;
-        for (int i = result.size() - 1; i >= 0; i--) {
-            result[i] += carry;
-            if (result[i] > 9) {
-                carry = result[i] - 9;
-                result[i] = result[i] % 10;
-            } else {
-                carry = 0;
-            }
+        int carry = 0;
+        for (int i = 0; i < n; i++) {
+            digits[i] += carry;
+            carry = digits[i] / 10;
+            digits[i] -= carry * 10;
         }
         
-        if (result[0] > 0) {
-            return result;
-        } else {
-            //Remove the 0 at the most significant digit
-            for (int i = 1; i < result.size(); i++)
-                digits[i-1] = result[i];
-                
-            return digits;
+        if (carry > 0) {
+            digits.push_back(carry);
         }
+        
+        reverse(digits.begin(), digits.end());
+        
+        return digits;
     }
 };

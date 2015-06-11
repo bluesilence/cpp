7c2cb0a5469d412ac02ad17bb9db32a9bb55d3be
bluesilence <bluesilencemail@126.com>
2015-06-11 21:33:48 +0800

    Update gray-code.cpp
    Second time.

--- LeetCode/gray-code.cpp
+++ LeetCode/gray-code.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<int> result;
-        result.push_back(0);
-        for(int i = 0; i < n; i++){
-            int highest = 1<<i;
-            int len = result.size();
-            for(int j = len - 1; j >= 0; j--){
-                result.push_back(highest + result[j]);
+        //The digit 1~n is set to 1 in order
+        //For i <- 1 to n, for the digits other than highest digits, the order is the reverse order of them in results
+        vector<int> results;
+        
+        if (n <= 0)
+            return results;
+        
+        results.push_back(0);
+        
+        for (int i = 0; i < n; i++) {
+            int highestDigit = 1 << i;
+            
+            for (int j = results.size() - 1; j >= 0; j--) {
+                results.push_back(highestDigit + results[j]);
             }
         }
         
-        return result;
+        return results;
     }
 };

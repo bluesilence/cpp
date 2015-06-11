a2991ef5e934e75e4b352e7b6730d2c1b974ad9d
bluesilence <bluesilencemail@126.com>
2015-06-11 20:57:35 +0800

    Update largest-rectangle-in-histogram.cpp
    Second time.
    Note:
    1. L8
    2. L19
    3. L22

--- LeetCode/largest-rectangle-in-histogram.cpp
+++ LeetCode/largest-rectangle-in-histogram.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int> &height) {
-        if(height.size() == 0)
+    int largestRectangleArea(vector<int>& height) {
+        if (height.empty())
             return 0;
-         
-        int result = 0;
-        vector<int> tmp = height;
-        tmp.push_back(0);  // Lower bound
- 
-        stack<int> s;
-        for(int i = 0; i < tmp.size(); i++) {
-            //1) Not met histograms higher than current histogram, or
-            //2) The height of left histogram < the height of current histogram
-            if(s.empty() || (!s.empty() && tmp[i] >= tmp[s.top()])) { 
-                s.push(i);
+        
+        //sentinal
+        height.push_back(0);
+        
+        int maxArea = 0;
+        int tmpArea = 0;
+        
+        stack<int> beginIndices;
+        
+        for (int i = 0; i <= height.size(); i++) {
+            if (beginIndices.empty() || height[i] >= height[beginIndices.top()]) {
+                beginIndices.push(i);
             } else {
-                while(!s.empty() && tmp[s.top()] > tmp[i]) {
-                    int index = s.top();
-                    s.pop();
-                    // Note it's s.top() in (i - s.top() - 1)
-                    // s.top() is the index of left histogram that has height < the height of histogram at index
-                    int width = s.empty() ? i : (i - s.top() - 1);
-                    result = max(result, tmp[index] * width);
+                while (!beginIndices.empty() && height[i] < height[beginIndices.top()]) {   //Otherwise, beginIndices.top() and i can form another ascending sequence
+                    int beginIndex = beginIndices.top();
+                    beginIndices.pop();
+                    int w = beginIndices.empty() ? i : (i - beginIndices.top() - 1);
+                    
+                    tmpArea = w * height[beginIndex];
+                    maxArea = max(maxArea, tmpArea);
                 }
                 
-                s.push(i);  // Important
+                beginIndices.push(i);
             }
         }
         
-        return result;
+        height.pop_back();
+        
+        return maxArea;
     }
 };

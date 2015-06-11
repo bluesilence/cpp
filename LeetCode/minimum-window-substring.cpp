456c448157673b29f738705f4a2e1a5989afc611
bluesilence <bluesilencemail@126.com>
2015-06-11 08:47:34 +0800

    Update minimum-window-substring.cpp
    Second time.
    Note:
    1. Only shrink the window when all unique chars in T are found in S;
    2. 2 pointers: i to move forward, begin to restrict the windows' width.

--- LeetCode/minimum-window-substring.cpp
+++ LeetCode/minimum-window-substring.cpp
@@ -1,62 +1,57 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (t.size() > s.size())
+        if (s.size() < t.size())
             return "";
         
-        if (t == s)
-            return s;
+        vector<int> charCountInS(256, 0);
+        vector<int> charCountInT(256, 0);
         
-        int alphaCountS[256];
-        int alphaCountT[256];
-        int uniqueAlphaCountT = 0;
-        int allAlphaFoundCountS = 0;
-        
-        memset(alphaCountS, 0, sizeof(alphaCountS));
-        memset(alphaCountT, 0, sizeof(alphaCountT));
+        int uniqueCharsInT = 0;
         
         for (int i = 0; i < t.size(); i++) {
-            if (alphaCountT[t[i]] == 0)
-                uniqueAlphaCountT++;
+            if (charCountInT[t[i]] == 0) {
+                uniqueCharsInT++;
+            }
             
-            alphaCountT[t[i]]++;
+            charCountInT[t[i]]++;
         }
         
-        int start = 0;
+        int foundUniqueTCharsInS = 0;
+        int begin = 0;
+        int minBegin = -1;
         int minWidth = s.size();
-        int minStart = -1;
         
         for (int i = 0; i < s.size(); i++) {
-            if (alphaCountT[s[i]] == 0) //The character hasn't appeared in t
+            char c = s[i];
+            if (charCountInT[c] == 0) {
                 continue;
-                
-            alphaCountS[s[i]]++;
-            //Once allAlphaFoundCountS == uniqueAlphaCountT, in the expansion to right, we always have alphaCountS[s[i]] > alphaCountT[s[i]]
-            //If alphaCountS[s[i]] > alphaCountT[s[i]], just go to L38 and contract left boundary to right
-            if (alphaCountS[s[i]] == alphaCountT[s[i]])
-                allAlphaFoundCountS++;
+            }
             
-            if (allAlphaFoundCountS == uniqueAlphaCountT) { //Found all alphas in target
-                //Even if alphaCountS[s[i]] > alphaCountT[s[i]], no need to contract window with regard to j
-                //Because in this case, (i - start + 1) >= minWidth, the global minimum won't be affected
-                while (alphaCountT[s[start]] == 0 || alphaCountS[s[start]] > alphaCountT[s[start]]) {   //Move left pointer to right
-                    if (alphaCountS[s[start]] > 0)
-                        alphaCountS[s[start]]--;
-                        
-                    start++;
+            charCountInS[c]++;
+            
+            if (charCountInS[c] == charCountInT[c]) {
+                foundUniqueTCharsInS++;
+            }
+            
+            if (foundUniqueTCharsInS == uniqueCharsInT) {
+                while (charCountInT[s[begin]] == 0 || charCountInS[s[begin]] > charCountInT[s[begin]]) {
+                    if (charCountInS[s[begin]] > 0)
+                        charCountInS[s[begin]]--;
+                            
+                    begin++;
                 }
-                
-                //Important: (minStart == -1) is added here, otherwise if (i - start + 1) == s.size(), minStart won't be updated 
-                if (minStart == -1 || i - start + 1 < minWidth) {   
-                    minWidth = i - start + 1;
-                    minStart = start;
+                    
+                if (minBegin == -1 || minWidth > (i - begin + 1)) {
+                    minBegin = begin;
+                    minWidth = i - begin + 1;
                 }
             }
         }
         
-        if (minStart == -1)
+        if (minBegin == -1)
             return "";
         else
-            return s.substr(minStart, minWidth);
+            return s.substr(minBegin, minWidth);
     }
 };

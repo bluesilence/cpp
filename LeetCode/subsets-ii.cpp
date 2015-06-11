af4d6bc2ba9e1241f4ab7d5ce7ebddba12c072e9
bluesilence <bluesilencemail@126.com>
2015-06-11 22:03:12 +0800

    Update subsets-ii.cpp
    Second time.
    Use a set to dedup.

--- LeetCode/subsets-ii.cpp
+++ LeetCode/subsets-ii.cpp
@@ -1,24 +1,44 @@
 class Solution {
 public:
-    vector<vector<int> > subsetsWithDup(vector<int> &S) {
-        vector<vector<int> > results;
-        vector<int> permutation;
-        std::sort(S.begin(), S.end());
-        subsetsCore(S, permutation, results, 0);
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> results;
+        
+        if (nums.empty())
+            return results;
+        
+        //Sort so that results appear in non-descending order
+        sort(nums.begin(), nums.end());
+        
+        vector<int> result;
+        set<string> distinctResults;
+        
+        pickCore(results, result, distinctResults, nums, 0);
         
         return results;
     }
-    
+
 private:
-    void subsetsCore(vector<int> &S, vector<int> &perm, vector<vector<int> > &results, int index) {
-        if (index == S.size()) {
-            if (results.empty() || results.end() == std::find(results.begin(), results.end(), perm))
-                results.push_back(perm);
+    void pickCore(vector<vector<int>> &results, vector<int> &result, set<string> &distinctResults, vector<int> &nums, int start) {
+        if (start == nums.size()) {
+            string s = formString(result);
+            if (distinctResults.find(s) == distinctResults.end()) {
+                results.push_back(result);
+                distinctResults.insert(s);
+            }
         } else {
-            perm.push_back(S[index]);
-            subsetsCore(S, perm, results, index + 1);
-            perm.pop_back();
-            subsetsCore(S, perm, results, index + 1);
+            result.push_back(nums[start]);
+            pickCore(results, result, distinctResults, nums, start + 1);
+            result.pop_back();
+            pickCore(results, result, distinctResults, nums, start + 1);
         }
+    }
+    
+    string formString(vector<int> &result) {
+        string s = "";
+        for (int i = 0; i < result.size(); i++) {
+            s += result[i];
+        }
+        
+        return s;
     }
 };

4b64441d728611d35ba96c1c7f897af971955dc0
bluesilence <bluesilencemail@126.com>
2015-06-12 20:47:01 +0800

    Update interleaving-string.cpp
    Second time.
    Note:
    1. L25~32: Boundary conditions;
    2. L36~37: Where the index starts.

--- LeetCode/interleaving-string.cpp
+++ LeetCode/interleaving-string.cpp
@@ -1,35 +1,48 @@
 class Solution {
 public:
-    //DP. define f[i][j]: if s3(1, i+j) interleaved by s1(1,i) s2(1,j)?  
-    //(1)then the transform equation is below:   
-    //if s1[i] == s3[i+j], f[i][j] = f[i-1][j]  
-    //if s2[j] == s3[i+j], f[i][j] = f[i][j-1]  
-    //(2)Initialize as below :  
-    //f[0][0] = true,  
-    //f[i][0] = f[i-1][0] && (s1[i-1] == s3[i-1]);  
-    //f[0][j] = f[0][j-1] && (s2[j-1] == s3[j-1]);  
     bool isInterleave(string s1, string s2, string s3) {
-        if (s1.empty() && (s2 == s3) || s2.empty() && (s1 == s3))
-            return true;
+        //DP. define f[i][j]: if s3(0, i+j-1) interleaved by s1(0, i-1) and s2(0, j-1)?  
+        //(1)then the transform equation is below:   
+        //if s1[i] == s3[i+j], f[i][j] = f[i][j] || f[i-1][j]  
+        //if s2[j] == s3[i+j], f[i][j] = f[i][j] || f[i][j-1]  
+        //(2)Initialize as below :  
+        //f[0][0] = true,  
+        //f[i][0] = f[i-1][0] && (s1[i-1] == s3[i-1]);  
+        //f[0][j] = f[0][j-1] && (s2[j-1] == s3[j-1]);
         
-        int l1 = s1.size(), l2 = s2.size(), l3 = s3.size();
-        if (l1 + l2 != l3)
+        const int m = s1.size();
+        const int n = s2.size();
+        const int k = s3.size();
+        
+        if (m + n != k)
             return false;
         
-        vector<vector<bool> > f(l1 + 1, vector<bool>(l2 + 1, false));
+        if (k == 0)
+            return true;
         
-        f[0][0] = true;  
-        for(int i = 0; i <= l1; i++)  {  
-            for(int j = 0; j <= l2; j++)  {  
-                int len = i + j;  
-                if(i >= 1 && s1[i - 1] == s3[len - 1])
-                    f[i][j] = f[i][j] || f[i - 1][j];
-                    
-                if(j >= 1 && s2[j - 1] == s3[len - 1])
-                    f[i][j] = f[i][j] || f[i][j - 1];  
-            }  
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        
+        dp[0][0] = true;
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = dp[i-1][0] && s3[i-1] == s1[i-1];
         }
         
-        return f[l1][l2];
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = dp[0][j-1] && s3[j-1] == s2[j-1];
+        }
+        
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i > 0 && s1[i-1] == s3[i+j-1]) {
+                    dp[i][j] = dp[i][j] || dp[i-1][j];  //Note: index of s starts from 0, while index of dp starts from 1
+                }
+                
+                if (j > 0 && s2[j-1] == s3[i+j-1]) {
+                    dp[i][j] = dp[i][j] || dp[i][j-1];
+                }
+            }
+        }
+        
+        return dp[m][n];
     }
 };

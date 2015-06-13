b304bb3e842710fd78bd8aaeaac6968a5c2e4683
bluesilence <bluesilencemail@126.com>
2015-06-13 22:21:47 +0800

    Update length-of-last-word.cpp
    Second time.

--- LeetCode/length-of-last-word.cpp
+++ LeetCode/length-of-last-word.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    int lengthOfLastWord(const char *s) {
-        if (!s || *s == '\0')
+    int lengthOfLastWord(string s) {
+        if (s.empty())
             return 0;
             
-        //Move pointer to the begin and end of the last word
-        int endIndex = 0;
-        while (s[endIndex] != '\0')
-            endIndex++;
-            
-        endIndex--;
-        while (endIndex >= 0 && s[endIndex] == ' ') //Exclude space at the end
-            endIndex--;
+        int lastWordBegin = 0;
+        int lastWordEnd = s.size() - 1;
         
-        if (-1 == endIndex) {   //Empty string with only spaces
+        for (; lastWordEnd >= 0 && s[lastWordEnd] == ' '; lastWordEnd--);
+        
+        if (lastWordEnd < 0)
             return 0;
-        } else {
-            endIndex++;
-            int beginIndex = endIndex - 1;
-            while (beginIndex >= 0 && s[beginIndex] != ' ')
-                beginIndex--;
-            
-            //Consider special case where there is only 1 word and no space
-            if (-1 == beginIndex)
-                return endIndex;
-            else
-                return endIndex - beginIndex - 1;
-        }
+        
+        for (lastWordBegin = lastWordEnd - 1; lastWordBegin >= 0 && s[lastWordBegin] != ' '; lastWordBegin--);
+        
+        return lastWordEnd - lastWordBegin;
     }
 };

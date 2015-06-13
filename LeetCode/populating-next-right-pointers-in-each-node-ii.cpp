88fc49490f3fc458b21b1748cae422fced0b793d
bluesilence <bluesilencemail@126.com>
2015-06-13 09:03:31 +0800

    Update populating-next-right-pointers-in-each-node-ii.cpp
    Second time.
    Note:
    L29~30: Need 2 ifs for tmp == NULL and q.empty() respectively, otherwise the tmp == NULL may enter the else in L35 and cause Runtime error.

--- LeetCode/populating-next-right-pointers-in-each-node-ii.cpp
+++ LeetCode/populating-next-right-pointers-in-each-node-ii.cpp
@@ -6,56 +6,43 @@
  *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
  * };
  */
-// Exactly the same as version i
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
         if (!root)
             return;
-            
-        //Level traverse
-        queue<TreeLinkNode*> q1, q2;
-        q1.push(root);
         
-        TreeLinkNode* pre = NULL, *curr = NULL;
-        while (!q1.empty() || !q2.empty()) {
-            pre = NULL;
-            if (q1.empty()) {
-                while (!q2.empty()) {
-                    curr = q2.front();
-                    if (curr->left)
-                        q1.push(curr->left);
-                    
-                    if (curr->right)
-                        q1.push(curr->right);
-                    
-                    q2.pop();
-                    if (pre) {
-                        pre->next = curr;
-                    }
+        queue<TreeLinkNode*> q;
+        TreeLinkNode *pre = NULL;
+        
+        q.push(root);
+        q.push(NULL);
+        
+        while (!q.empty()) {
+            TreeLinkNode *tmp = q.front();
+            q.pop();
+            
+            if (pre != NULL) {
+                pre->next = tmp;
+            }
+            
+            if (tmp == NULL) {
+                if (!q.empty()) {   //There are more layers to visit
+                    pre = NULL;
                 
-                    pre = curr;
+                    q.push(NULL);  //Insert terminator of next layer
                 }
             } else {
-                 while (!q1.empty()) {
-                    curr = q1.front();
-                    if (curr->left)
-                        q2.push(curr->left);
-                    
-                    if (curr->right)
-                        q2.push(curr->right);
-                    
-                    q1.pop();
-                    if (pre) {
-                        pre->next = curr;
-                    }
+                pre = tmp;
                 
-                    pre = curr;
+                if (tmp->left) {
+                    q.push(tmp->left);
+                }
+            
+                if (tmp->right) {
+                    q.push(tmp->right);
                 }
             }
-                
-            if (pre)
-                pre->next = NULL;
         }
     }
 };

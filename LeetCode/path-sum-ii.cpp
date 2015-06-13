5cad33d4111393835f4f9cb7496ce2ba82dcefcc
bluesilence <bluesilencemail@126.com>
2015-06-13 08:21:02 +0800

    Update path-sum-ii.cpp
    Second time.

--- LeetCode/path-sum-ii.cpp
+++ LeetCode/path-sum-ii.cpp
@@ -1,5 +1,5 @@
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
@@ -9,42 +9,37 @@
  */
 class Solution {
 public:
-    vector<vector<int> > pathSum(TreeNode *root, int sum) {
-        vector<vector<int> > results;
-        vector<int> path;
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>> results;
+        
         if (!root)
             return results;
-        else
-            pathSumCore(root, sum, path, results);
         
-        return results;
+        vector<int> result;
+        pathSumCore(results, result, root, sum);
     }
-    
+
 private:
-    void pathSumCore(TreeNode *root, int sum, vector<int> &path, vector<vector<int> >& results) {
-        if (!root->left && !root->right) {  //Is leaf node
-            if (root->val == sum) { //Found a path
-                path.push_back(root->val);
-                results.push_back(path);
-                path.pop_back();    //Remove current leaf node to try another way
-            } else {    //This way is no way
-                return;
+    void pathSumCore(vector<vector<int>> &results, vector<int> &result, TreeNode *root, int sum) {
+        if (!root)
+            return;
+        
+        result.push_back(root->val);
+        
+        if (!root->left && !root->right) {
+            if (root->val == sum) {
+                results.push_back(result);
             }
         } else {
-            //Going this way
-            path.push_back(root->val);
+            if (root->left) {
+                pathSumCore(results, result, root->left, sum - root->val);
+            }
             
-            //Going down to left
-            if (root->left)
-                pathSumCore(root->left, sum - root->val, path, results);
-                
-            //Going down to right
-            if (root->right)
-                pathSumCore(root->right, sum - root->val, path, results);
-            
-            //Not going this way
-            path.pop_back();
+            if (root->right) {
+                pathSumCore(results, result, root->right, sum - root->val);
+            }
         }
-            
+        
+        result.pop_back();
     }
 };

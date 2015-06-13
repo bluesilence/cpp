af4c095bac5d0a38c237f61a34eb2648b5a19bfa
bluesilence <bluesilencemail@126.com>
2015-06-13 09:42:19 +0800

    Update zigzag-conversion.cpp
    Second time.
    Make sure you understand the format of zigzag, then code.

--- LeetCode/zigzag-conversion.cpp
+++ LeetCode/zigzag-conversion.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    string convert(string s, int nRows) {
-        if (nRows == 1) {
+    string convert(string s, int numRows) {
+        if (s.size() <= numRows || numRows <= 1)
             return s;
-        }
         
-        string temp[nRows];
+        string tmpRows[numRows];
         
-        int idx = -1, step = 1;
+        int row = -1;
+        int step = 1;
         
         for (int i = 0; i < s.size(); i++) {
-            //Next row to append
-            idx += step;
+            row += step;
             
-            //Out of bound
-            if (idx == nRows) {
-                idx = nRows - 2;
+            if (row >= numRows) {
+                row = numRows - 2;  //Note: it's "- 2", not "- 1"
                 step = -1;
-            } else if (idx == -1) { //Out of bound reversely
-                idx = 1;
+            } else if (row < 0) {
+                row = 1;
                 step = 1;
             }
             
-            temp[idx] += s[i];
+            tmpRows[row] += s[i];
         }
         
-        string ans = temp[0];
-        for (int i = 1; i < nRows; i++) {
-            ans += temp[i];
+        string result = "";
+        for (int i = 0; i < numRows; i++) {
+            result += tmpRows[i];
         }
         
-        return ans;
+        return result;
     }
 };

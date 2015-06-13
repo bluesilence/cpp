75b60e464f6b24a35a568c52b9bc5c2f4dda08ea
bluesilence <bluesilencemail@126.com>
2015-06-13 10:08:43 +0800

    Update 3sum-closest.cpp
    Second time.
    Note:
    1. L15, L34
    2. L21: use abs to measure diff
    3. L29: since target has subtracted nums[i], here we need to add it back

--- LeetCode/3sum-closest.cpp
+++ LeetCode/3sum-closest.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    int threeSumClosest(vector<int> &num, int target) {
-        sort(num.begin(), num.end());
+    int threeSumClosest(vector<int>& nums, int target) {
+        if (nums.size() < 3)
+            throw "Size of nums must be >= 3.";
         
-        int closestSum = 0;
-        int numSize = num.size();
-        int tmpSum = 0;
-        bool isFirstSum = true;
+        int minDiff = numeric_limits<int>::max();
         
+        sort(nums.begin(), nums.end());
         
-        for (int i = 0; i < numSize; i++) {
+        for (int i = 0; i + 2 < nums.size(); i++) {
             int j = i + 1;
-            int k = num.size() - 1;
+            int k = nums.size() - 1;
             
-            while(j < k) {
-                tmpSum = num[i] + num[j] + num[k];
-                if (isFirstSum) {
-                    closestSum = tmpSum;
-                    isFirstSum = false;
-                } else if (abs(tmpSum - target) < abs(closestSum - target)) {
-                    closestSum = tmpSum;                                         
+            target -= nums[i];
+            
+            while (j < k) {
+                int sum = nums[j] + nums[k];
+                int diff = target - sum;
+                
+                if (abs(diff) < abs(minDiff)) {
+                    minDiff = diff;
                 }
                 
-                //Found closest, no need to continue
-                if (closestSum == target)
-                    return closestSum;
-                
-                //Similar to 2-partitioned search, move index according to the difference between current sum and target
-                if (tmpSum > target)
-                    k--;
-                else
+                if (diff > 0) {
                     j++;
+                } else if (diff < 0) {
+                    k--;
+                } else {    //Get global minimum
+                    return nums[i] + target;
+                }
             }
+            
+            target += nums[i];
         }
         
-        return closestSum;
+        return target - minDiff;
     }
 };

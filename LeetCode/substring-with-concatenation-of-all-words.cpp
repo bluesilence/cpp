389317b2271bdc72a670f8dc5c7478287e32d6ad
bluesilence <bluesilencemail@126.com>
2015-06-13 12:51:14 +0800

    Update substring-with-concatenation-of-all-words.cpp
    Second time.
    Note:
    For all the questions with a search window, record the count in a map is a good choice.

--- LeetCode/substring-with-concatenation-of-all-words.cpp
+++ LeetCode/substring-with-concatenation-of-all-words.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
-    vector<int> findSubstring(string S, vector<string> &L) {
-		map<string, int> expectedCount;
-		map<string, int> occurredCount;
-		
-		int wordNum = L.size();
-		int wordLen = L[0].size();
-		
-		for (int i = 0; i < wordNum; i++)
-			expectedCount[L[i]]++;
-		
-		vector<int> results;
-		
-		for (int i = 0; i <= (int)S.size() - wordLen * wordNum; i++) {   //For all the possible starting index
-			occurredCount.clear();
-			int j;
-			for (j = 0; j < wordNum; j++) {  //Get the jth word starting from index i
-				string word = S.substr(i + j * wordLen, wordLen);
-				if (expectedCount.find(word) == expectedCount.end())    //The current word is not in the list
-					break;
-					
-				occurredCount[word]++;
-				if (occurredCount[word] > expectedCount[word])   //Too many occurrence of word will break the match
-					break;
-			}
-			
-			if (j == wordNum)
-				results.push_back(i);
-		}
-		
-		return results;
-	}
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> results;
+        if (words.empty() || s.size() < words[0].size())
+            return results;
+        
+        map<string, int> expectedCounts;
+        map<string, int> actualCounts;
+        
+        const int wordsNum = words.size();
+        const int wordLen = words[0].size();
+        
+        for (int i = 0; i < wordsNum; i++) {
+            expectedCounts[words[i]]++;
+        }
+        
+        for (int i = 0; i + wordsNum * wordLen <= s.size(); i++) {   //Starting index to find next substring
+            actualCounts.clear();
+            int j;
+            for (j = 0; j < wordsNum; j++) {
+                string substring = s.substr(i + j * wordLen, wordLen);
+                if (expectedCounts.find(substring) == expectedCounts.end()) {
+                    break;  //The substring doesn't exist in words
+                }
+                
+                actualCounts[substring]++;
+                
+                if (actualCounts[substring] > expectedCounts[substring]) {
+                    break;
+                }
+            }
+            
+            if (j == wordsNum) {    //Found all expected words
+                results.push_back(i);
+            }
+        }
+        
+        return results;
+    }
 };

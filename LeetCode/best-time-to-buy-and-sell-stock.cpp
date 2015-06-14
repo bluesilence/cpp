1d7cb9d172666f19255964b7643b288e6d83cfa3
bluesilence <bluesilencemail@126.com>
2015-06-14 13:58:09 +0800

    Update best-time-to-buy-and-sell-stock.cpp
    Second time.

--- LeetCode/best-time-to-buy-and-sell-stock.cpp
+++ LeetCode/best-time-to-buy-and-sell-stock.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    int maxProfit(vector<int> &prices) {
-        //Find the maximum difference between 2 prices
-        //The smaller price forward, the bigger price backward
-        //If there is no > 0 difference, better avoid transaction and return 0 as max profit
-        int profit = 0;
-        if (prices.size() > 1) {
-            int end = prices.size() - 1;
-            vector<int> mins(prices.size(), 0);
-            vector<int> maxs(prices.size(), 0);
-            mins[0] = prices[0];
-            maxs[end] = prices[end];
+    int maxProfit(vector<int>& prices) {
+        //Define 2 vectors, 1 for the lowest price before day i, 1 for the highest price after day i
+        //The max profit is max(highest[i] - lowest[i])
+        const int N = prices.size();
+        
+        if (N < 2)  //You cannot buy and sell at the same day
+            return 0;
             
-            for (int i = 1; i <= end; i++) {
-                if (prices[i] < mins[i-1])
-                    mins[i] = prices[i];
-                else
-                    mins[i] = mins[i-1];
-            }
-            
-            for (int j = end - 1; j >= 0; j--) {
-                if (prices[j] > maxs[j+1])
-                    maxs[j] = prices[j];
-                else
-                    maxs[j] = maxs[j+1];
-            }
-            
-            for (int i = 0; i <= end; i++) {
-                if (maxs[i] - mins[i] > profit)
-                    profit = maxs[i] - mins[i];
-            }
+        vector<int> lowestPrices(N, 0);
+        vector<int> highestPrices(N, 0);
+        
+        lowestPrices[0] = prices[0];
+        highestPrices[N-1] = prices[N-1];
+        
+        for (int i = 1; i < N; i++) {
+            lowestPrices[i] = min(lowestPrices[i-1], prices[i]);
         }
         
-        return profit;
+        for (int i = N-2; i >= 0; i--) {
+            highestPrices[i] = max(highestPrices[i+1], prices[i]);
+        }
+        
+        int maxProfit = 0;
+        
+        for (int i = 0; i < N; i++) {
+            maxProfit = max(maxProfit, highestPrices[i] - lowestPrices[i]);
+        }
+        
+        return maxProfit;
     }
 };

e7f554a75dda33ec013367c472360f35c8b93aee
bluesilence <bluesilencemail@126.com>
2015-06-14 08:26:56 +0800

    Update permutation-sequence.cpp
    Second time.
    The mathematical solution is easier to understand.

--- LeetCode/permutation-sequence.cpp
+++ LeetCode/permutation-sequence.cpp
@@ -1,51 +1,43 @@
-#include <vector>;
-#include <algorithm>;
-#include <string>;
-#include <iostream>;
-using namespace std;
-
-/*
- */
 class Solution {
 public:
     string getPermutation(int n, int k) {
-       //Form the sequence from 1 to n
-       string origin;
-       for (int i = 1; i <= n; i++) {
-           origin += i + '0';
-       }
-       
-       //Generate all possible permutations
-       vector<string> permutations;
-       genPermutations(origin, 0, permutations);
-       
-       //Return the kth sequence in the sorted array
-       if (k > permutations.size() || k < 1) {  //k starts from 1?
-           return "";
-       } else {
-           //Sort the permutations ascendantly
-           sort(permutations.begin(), permutations.end());
-           return permutations[k - 1];
-       }
-    }
-    
-    void genPermutations(string &str, int index, vector<string> &results) {
-        if (str.size() <= index) { //Come to the end, collect the string
-            results.push_back(str);
-        } else {
-            for (int i = index; i < str.size(); i++) {
-                swap(str, index, i);
-                genPermutations(str, index + 1, results);
-                swap(str, index, i);
+        //The highest digit is in order: 1~n, with (n-1)! repeats for each
+        //The second highest digit is in order: 1~n except the highest digit, with (n-2)! repeats for each
+        //Suppose the kth permutation is (a1, a2, a3, ..., an), there are in total n! permutations
+        //The (n-1) elements except the highest digit have (n-1)! permutations
+        //Set k1 = k, then a1 = k1 / (n-1)!
+        //Set k2 = k1 % (n-1)!, then a2 = k2 / (n-2)!
+        //And so on...
+        //Set k(n-1) = k(n-2) % 2!, then a(n-1) = k(n-1) / 1!
+        //Set k(n) = k(n-1) % 1!, then a(n) = k(n) / 0!
+        if (n < 1 || k < 1)
+            throw "n and k must be > 0";
+            
+        string result = "";
+        
+        int factorial = 1;  //(n-1)!
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        
+        vector<int> nums(n, 0);
+        
+        for (int i = 0; i < n; i++) {
+            nums[i] = i + 1;
+        }
+        
+		k--;	//The index starts from 0
+        for (int i = 0; i < n; i++) {
+            factorial /= n - i; //(n-i)! => (n-i-1)!
+            int j = k / factorial;
+            result += nums[j] + '0';    //a(i)
+            k %= factorial; //k(i+1) = k(i) % (n-i)!
+            
+            for (; j + 1 < n - i; j++) {
+                nums[j] = nums[j+1];
             }
         }
-    }
-    
-    void swap(string &str, int indexA, int indexB) {
-        if (indexA != indexB && indexA >= 0 && indexB >=0 && indexA < str.size() && indexB < str.size()) {
-            char tmp = str[indexA];
-            str[indexA] = str[indexB];
-            str[indexB] = tmp;
-        }
+        
+        return result;
     }
 };

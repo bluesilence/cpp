2b948edd70ba9ff3183f13a010c814dedd4939aa
bluesilence <bluesilencemail@126.com>
2015-06-14 14:37:55 +0800

    Update best-time-to-buy-and-sell-stock-iii.cpp
    Second time.

--- LeetCode/best-time-to-buy-and-sell-stock-iii.cpp
+++ LeetCode/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-/*
-解释：
-首先，因为能买2次（第一次的卖可以和第二次的买在同一时间），但第二次的买不能在第一次的卖左边。
-所以维护2个表，f1和f2，size都和prices一样大。
-意义：
-f1[i]表示 -- 截止到i下标为止，左边所做交易能够达到最大profit；
-f2[i]表示 -- 截止到i下标为止，右边所做交易能够达到最大profit；
-那么，对于f1 + f2，寻求最大即可。
-*/
-    int maxProfit(vector<int> &prices) {
-        int size = prices.size();
-        if (size == 0)
+    int maxProfit(vector<int>& prices) {
+        //Since there can be 2 transactions, define 2 vectors leftProfits & rightProfits
+        //leftProfits[i]: the maxProfit of 1 transaction made before day i(inclusive)
+        //rightProfits[i]: the maxProfit of 1 transaction made after day i(inclusive)
+        //At day i, we can sell stock then buy again
+        const int N = prices.size();
+        if (N < 2)
             return 0;
-
-        vector<int> leftProfit(size);
-        vector<int> rightProfit(size);
-
-        int minV = prices[0];
-        for (int i = 1; i < size; i++){
-            minV = std::min(minV, prices[i]);
-            leftProfit[i] = std::max(leftProfit[i - 1], prices[i] - minV);
+        
+        vector<int> leftProfits(N, 0);
+        vector<int> rightProfits(N, 0);
+        
+        int minPrice = prices[0];
+        
+        for (int i = 1; i < N; i++) {
+            leftProfits[i] = max(leftProfits[i-1], prices[i] - minPrice);
+            minPrice = min(minPrice, prices[i]);
         }
-
-        int maxV = prices[size - 1];
-        rightProfit[size - 1] = 0;
-        for (int i = size - 2; i >= 0; i--){
-            maxV = std::max(maxV, prices[i]);
-            rightProfit[i] = std::max(rightProfit[i + 1], maxV - prices[i]);
+        
+        int maxPrice = prices[N-1];
+        
+        for (int i = N-2; i >= 0; i--) {
+            rightProfits[i] = max(rightProfits[i+1], maxPrice - prices[i]);
+            maxPrice = max(maxPrice, prices[i]);
         }
-
-        int sum = 0;
-        for (int i = 0; i < size; i++)
-            sum = std::max(sum, leftProfit[i] + rightProfit[i]);
-
-        return sum;
+        
+        int profit = 0;
+        for (int i = 0; i < N; i++) {
+            int tmpProfit = rightProfits[i] + leftProfits[i];
+            profit = max(tmpProfit, profit);
+        }
+        
+        return profit;
     }
 };

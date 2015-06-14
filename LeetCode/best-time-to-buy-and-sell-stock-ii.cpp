94c2850d8179038812f8fdc56ac19b406ab6868b
bluesilence <bluesilencemail@126.com>
2015-06-14 14:22:45 +0800

    Update best-time-to-buy-and-sell-stock-ii.cpp
    Second time.

--- LeetCode/best-time-to-buy-and-sell-stock-ii.cpp
+++ LeetCode/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    int maxProfit(vector<int> &prices) {
-        if(prices.empty())
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty())
             return 0;
-
-        int max = 0;
-        int start = prices[0];
-        int last = prices[0];
-        int n = prices.size();
-        for(int i = 1; i < n; i++) {
-            //Sum profits in each ascending profit range
-            if(prices[i] >= last) {
-                last = prices[i];
-                if(i == n - 1)  // end
-                    max += (last - start);
+        
+        int profit = 0;
+        
+        int ascendingStart = 0;
+        int ascendingEnd = 0;
+        
+        for (int i = 0; i < prices.size(); i++) {
+            if (prices[i] > prices[ascendingEnd]) {
+                ascendingEnd = i;
+                if (i == prices.size() - 1) {
+                    profit += prices[ascendingEnd] - prices[ascendingStart];
+                }
             } else {
-                max += (last - start);
-                start = prices[i];
-                last = prices[i];
+                profit += prices[ascendingEnd] - prices[ascendingStart];
+                ascendingStart = i;
+                ascendingEnd = ascendingStart;
             }
         }
         
-        return max;
+        return profit;
     }
 };

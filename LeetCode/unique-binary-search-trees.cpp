0aded5636cc87c5b61c93561c10aff884495146d
bluesilence <bluesilencemail@126.com>
2015-06-14 09:54:54 +0800

    Update unique-binary-search-trees.cpp
    Second time.
    Note:
    1. L8, dp[0] == 1, not 0
    2. L21, the nodes in left child == i - 1, the nodes in right child == n - i.

--- LeetCode/unique-binary-search-trees.cpp
+++ LeetCode/unique-binary-search-trees.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
     int numTrees(int n) {
-        int *dp = new int[n + 1];
+        if (n < 1)
+            return 0;
         
+        vector<int> dp(n + 1, -1);
         dp[0] = 1;
         dp[1] = 1;
-        for (int i = 2; i < n + 1; i++) {
-            dp[i] = -1;
-        }
         
-        int count = calcNum(dp, n);
-        
-        delete dp;
-        
-        return count;
+        return countUniques(dp, n);
     }
-    
-private:
-    int calcNum(int *dp, int num) {
-        if (dp[num] >= 0)   //Already calculated
-            return dp[num];
         
+private:
+    int countUniques(vector<int> &dp, int n) {
+        if (dp[n] > -1) //Already calculated
+            return dp[n];
+            
         int count = 0;
-        //The unique tree count related to one node depends on the product of counts of its all possible left and right subtrees
-        for (int i = 0; i < num; i++) {
-            count += calcNum(dp, i) * calcNum(dp, num - i - 1);
+        for (int i = 1; i <= n; i++) {
+            count += countUniques(dp, i - 1) * countUniques(dp, n - i);
         }
         
-        dp[num] = count;
+        dp[n] = count;
         
         return count;
     }

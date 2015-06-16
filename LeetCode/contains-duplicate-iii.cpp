9aa4ef1b4235291de90ac01bd3c2265ab789f18c
bluesilence <bluesilencemail@126.com>
2015-06-16 08:52:09 +0800

    Update contains-duplicate-iii.cpp

--- LeetCode/contains-duplicate-iii.cpp
+++ LeetCode/contains-duplicate-iii.cpp
@@ -7,8 +7,8 @@ public:
         if (k < 0 || t < 0)
             return false;
             
-		return containsNearbyAlmostDuplicateCore(nums, (long long)k, (long long)t);
-	}
+	return containsNearbyAlmostDuplicateCore(nums, (long long)k, (long long)t);
+}
 
 private:
 	bool containsNearbyAlmostDuplicateCore(vector<int>& nums, long long k, long long t) {
@@ -22,12 +22,12 @@ private:
             }
             
             //Is there any number in the set which >= nums[i] - t?
-			auto iter = latestNums.lower_bound(nums[i] - t);
-			//The iter from set is internally sorted, so just need to check the first number
-			//If this number > nums[i] + t, then there is no number that drops into [num[i]-t, nums[i]+t];
-			if (iter != latestNums.end() && *iter <= nums[i] + t) {
-			    return true;
-			}
+	    auto iter = latestNums.lower_bound(nums[i] - t);
+	    //The iter from set is internally sorted, so just need to check the first number
+	    //If this number > nums[i] + t, then there is no number that drops into [num[i]-t, nums[i]+t];
+	    if (iter != latestNums.end() && *iter <= nums[i] + t) {
+		return true;
+	    }
             
             latestNums.insert(nums[i]);
         }

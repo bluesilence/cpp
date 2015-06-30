86f5749038d3d112ceb8fcd2178ff44bd5d23735
bluesilence <bluesilencemail@126.com>
2015-06-30 09:40:12 +0800

    Update basic-calculator.cpp
    A shorter version.

--- LeetCode/basic-calculator.cpp
+++ LeetCode/basic-calculator.cpp
@@ -4,32 +4,31 @@ public:
         if (s.empty())
             return 0;
         
-        s = trim(s);
         s = "+(" + s + ")";
         
         long long result = 0;
         long long additiveNum = 0;
         
         int currentSign = 1;    //Default sign
-        //Store the absolute sign for each level of "()" block.
         stack<int> signs;
         signs.push(1);  //Default sign
         
         char op;
+        
+        //Skip ' ' during the loop
         for (int i = 0; i < s.size(); i++) {
             char ch = s[i];
             if (ch == '+' || ch == '-') {
-                if (i > 0 && s[i-1] >= '0' && s[i-1] <= '9') {
-                    //sign of a number is determined by direct sign and the sign before its '('
-                    additiveNum *= signs.top() * currentSign; //Caution: INT_MIN * (-1) will overflow. Use long long here.
-                    result += additiveNum;
-                    additiveNum = 0;
-                }
+                //sign of a number is determined by direct sign and the sign before its '('
+                //If additiveNum is already added within ')' if-branch, it will be set to 0 and no affect to result here
+                additiveNum *= signs.top() * currentSign; //Caution: INT_MIN * (-1) will overflow. Use long long here.
+                result += additiveNum;
+                additiveNum = 0;
                 
                 currentSign = ch == '-' ? -1 : 1;
             } else if (ch == '(' || ch == ')') {
                 if (ch == '(') {
-                    signs.push(signs.top() * currentSign);    //The sign for this "()" block is determined by the last sign at this level * the sign before '('
+                    signs.push(signs.top() * currentSign);    //The sign before '('
                     currentSign = 1;    //Initialize for the substring within ()
                 } else {
                     additiveNum *= signs.top() * currentSign; //Caution: INT_MIN * (-1) will overflow. Use long long here.
@@ -37,7 +36,7 @@ public:
                     additiveNum = 0;
                     signs.pop();
                 }
-            } else {
+            } else if (ch >= '0' && ch <= '9') {
                 additiveNum *= 10;
                 additiveNum += ch - '0';
             }
@@ -53,18 +52,5 @@ public:
         } else {
             return (int)result;
         }
-    }
-
-private:
-    string trim(string &s) {
-        string result = "";
-        
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] != ' ') {
-                result.push_back(s[i]);
-            }
-        }
-        
-        return result;
     }
 };

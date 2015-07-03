fd16d8a223a5410af1baf174d6d0abb53ab93ec0
bluesilence <bluesilencemail@126.com>
2015-07-03 09:36:43 +0800

    Update candy.cpp
    Second time.
    Best solution:
    https://leetcode.com/discuss/16463/a-simple-solution

--- LeetCode/candy.cpp
+++ LeetCode/candy.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int candy(vector<int> &ratings) {
-        //Similar to trap water question
-        //First, give each child 1 candy
-        //Second, scan the children line
-        //If a child has higher rating value than at least one of the neighbors, give the child one more candy.
+    int candy(vector<int>& ratings) {
         if (ratings.empty())
             return 0;
         
-        int n = ratings.size();
-        vector<int> candies(ratings.size(), 1);
+        const int N = ratings.size();
         
-        //Scan left to right, ensure that every child with higher rating than left has more candies than the left neighbor
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1])
-                candies[i] = max(candies[i], candies[i - 1] + 1);
+        vector<int> candies(N, 1);
+        
+        //Give more candies to children rating higher than their left neighbor
+        for (int i = 1; i < N; i++) {
+            if (ratings[i] > ratings[i-1])
+                candies[i] = candies[i-1] + 1;
         }
         
-        //Scan right to left, ensure that every child with higher rating than right has more candies than the right neighbor
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1])
-                candies[i] = max(candies[i], candies[i + 1] + 1);
+        //Give more candies to children rating higher than their right neighbor, subject to candies assigned based on their left neighbor
+        for (int i = N-2; i >= 0; i--) {
+            if (ratings[i] > ratings[i+1])
+                candies[i] = max(candies[i], candies[i+1] + 1);
         }
         
         int result = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             result += candies[i];
         }
         

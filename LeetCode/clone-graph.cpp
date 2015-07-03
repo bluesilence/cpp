48309d9c3c8cb6780b05e91c32a81100910876b7
bluesilence <bluesilencemail@126.com>
2015-07-03 08:15:09 +0800

    Update clone-graph.cpp
    Second time.
    One pass:)

--- LeetCode/clone-graph.cpp
+++ LeetCode/clone-graph.cpp
@@ -1,54 +1,42 @@
-/** 
- * Definition for undirected graph. 
- * struct UndirectedGraphNode { 
- *     int label; 
- *     vector<UndirectedGraphNode *> neighbors; 
- *     UndirectedGraphNode(int x) : label(x) {}; 
- * }; 
- */  
-class Solution {  
-public:  
-    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {  
-        if(!node)
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
+class Solution {
+public:
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        if (!node)
             return NULL;
         
-        map<int, UndirectedGraphNode*> nodes;  
-        queue<UndirectedGraphNode*> q;  
+        UndirectedGraphNode *startNode = new UndirectedGraphNode(node->label);
         
-        //First BFS to copy nodes
+        unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> oldNewMap;
+        oldNewMap[node] = startNode;
+        
+        queue<UndirectedGraphNode*> q;
         q.push(node);
-        while(!q.empty())  {  
+        
+        while (!q.empty()) {
             UndirectedGraphNode* tmp = q.front();
             q.pop();
             
-            if(nodes.find(tmp->label) == nodes.end())  {  //Not visited
-                UndirectedGraphNode* new_node = new UndirectedGraphNode(tmp->label);
+            const int adjCount = tmp->neighbors.size();
+            for (int i = 0; i < adjCount; i++) {
+                if (oldNewMap.find(tmp->neighbors[i]) == oldNewMap.end()) {
+                    UndirectedGraphNode* newNode = new UndirectedGraphNode(tmp->neighbors[i]->label);
+                    oldNewMap[tmp->neighbors[i]] = newNode;
+                    
+                    q.push(tmp->neighbors[i]);
+                }
                 
-                //Update pointer of original node with that of new node
-                nodes[new_node->label] = new_node;
-                
-                for(int i = 0; i < tmp->neighbors.size(); i++)  {  
-                    q.push(tmp->neighbors[i]);  
-                }  
-            }  
-        }  
+                (oldNewMap[tmp]->neighbors).push_back(oldNewMap[tmp->neighbors[i]]);
+            }
+        }
         
-        
-        //Second BFS to copy neighbors
-        q.push(node);  
-        while(!q.empty())  {  
-            UndirectedGraphNode* tmp = q.front();  
-            q.pop();
-            
-            UndirectedGraphNode* existingnode = nodes[tmp->label];  
-            if(existingnode->neighbors.empty() && !tmp->neighbors.empty())  {   //Not visited
-                for(int i = 0; i < tmp->neighbors.size(); i++)  {  
-                    existingnode->neighbors.push_back(nodes[tmp->neighbors[i]->label]);  
-                    q.push(tmp->neighbors[i]);  
-                }  
-            }  
-        }  
-          
-        return nodes[node->label];  
-    }  
-};  
+        return startNode;
+    }
+};

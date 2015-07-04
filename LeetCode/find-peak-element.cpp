92a2c7194a5d3d869eaea5ef7b2780250f669d31
bluesilence <bluesilencemail@126.com>
2015-07-04 15:34:44 +0800

    Update find-peak-element.cpp
    Second time.
    Note:
    L8: consider a simple test case [0, 1]. The loop condition should be "<=" instead of "<".

--- LeetCode/find-peak-element.cpp
+++ LeetCode/find-peak-element.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    int findPeakElement(const vector<int> &num) {
-        if (num.empty())
-            return -1;
-            
-        int index = 0;
+    int findPeakElement(vector<int>& nums) {
+        int begin = 0;
+        int end = nums.size() - 1;
+        int mid;
         
-        for (int i = 1; i < num.size(); i++) {
-            if (num[i] > num[index])
-                index = i;
+        while (begin <= end) {
+            mid = (begin + end) / 2;
+            bool greaterThanLeft = (mid == 0) || (mid > 0 && nums[mid] > nums[mid-1]);
+            bool greaterThanRight = (mid + 1 == nums.size()) || (mid + 1 < nums.size() && nums[mid] > nums[mid+1]);
+            
+            if (greaterThanLeft && greaterThanRight)
+                return mid;
+            else if (greaterThanLeft)
+                begin = mid + 1;
+            else
+                end = mid - 1;
         }
         
-        return index;
+        return mid;
     }
 };

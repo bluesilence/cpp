3e2f2d773b8f2649ac71fa8231340661f3885166
bluesilence <bluesilencemail@126.com>
2015-07-04 15:10:15 +0800

    Update compare-version-numbers.cpp
    Second time.
    Note:
    L20: if one string is exhausted, return 0 as the current level of version to compare with the other string.

--- LeetCode/compare-version-numbers.cpp
+++ LeetCode/compare-version-numbers.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        int v1, v2;
-        int index1 = 0, index2 = 0;
-        v1 = getNextLevel(version1, index1);
-        v2 = getNextLevel(version2, index2);
-        while (index1 < version1.size() || index2 < version2.size()) {
-            if (v1 > v2)
-                return 1;
-            else if (v1 < v2)
-                return -1;
-            else {
-                v1 = getNextLevel(version1, index1);
-                v2 = getNextLevel(version2, index2);
-            }
-        }
+        const int LEN1 = version1.size();
+        const int LEN2 = version2.size();
         
-        //Last digit of the longer one of version 1 and 2
-        if (v1 == v2)
-            return 0;
-        else if (v1 > v2)
-            return 1;
-        else
-            return -1;
+        int index1 = 0;
+        int index2 = 0;
+        
+        int num1 = 0;
+        int num2 = 0;
+        
+        do {
+            num1 = getNextLevel(version1, index1);
+            num2 = getNextLevel(version2, index2);
+            
+            if (num1 != num2) {
+                return num1 > num2 ? 1 : -1;
+            }
+        } while (index1 <= LEN1 || index2 <= LEN2);
+        
+        return 0;
     }
 
 private:
-    int getNextLevel(string version, int &index) {
-        if (index >= version.size() || version[index] == '.')
+    //If the substring is like "..", return 0 as the current level of version
+    int getNextLevel(string s, int &begin) {
+        if (begin >= s.size())
             return 0;
         
         int result = 0;
-        while (index < version.size() && version[index] != '.') {
-            result = result * 10 + version[index] - '0';
-            index++;
+        
+        while (begin < s.size() && s[begin] != '.') {
+            result *= 10;
+            result += s[begin++] - '0';
         }
         
-        if (index < version.size())
-            //skip '.' and point to next level's version
-            index++;
+        begin++;
         
         return result;
     }

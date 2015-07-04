6313eebd33a1e38a9925a61721471b85c2155262
bluesilence <bluesilencemail@126.com>
2015-07-04 08:08:30 +0800

    Update single-number.cpp
    Second time.

--- LeetCode/single-number.cpp
+++ LeetCode/single-number.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
-    int singleNumber(int A[], int n) {
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            result ^= A[i];
+    int singleNumber(vector<int>& nums) {
+        int xorNum = nums[0];
+        
+        const int N = nums.size();
+        for (int i = 1; i < N; i++) {
+            xorNum ^= nums[i];
         }
         
-        return result;
+        return xorNum;
     }
 };

1c73adeacf92fa6e3e24e04e4a9813abf82dfd99
bluesilence <bluesilencemail@126.com>
2015-07-04 13:32:06 +0800

    Update add-binary.cpp
    Second time.

--- LeetCode/add-binary.cpp
+++ LeetCode/add-binary.cpp
@@ -1,33 +1,71 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        if ("0" == a)
+        if (a.empty())
             return b;
         
-        if ("0" == b)
+        if (b.empty())
             return a;
+        
+        reverse(a);
+        reverse(b);
+        
+        const int aLen = a.size();
+        const int bLen = b.size();
+        bool carry = false;
+        
+        const int LEN = max(aLen, bLen);
+        
+        string result(LEN, '0');
+        
+        for (int i = 0; i < aLen; i++) {
+            result[i] = a[i];
+        }
+        
+        for (int i = 0; i < LEN; i++) {
+            if (i < bLen)
+                result[i] = addDigit(result[i], b[i], carry);
+            else
+                result[i] = addDigit(result[i], '0', carry);
+        }
+        
+        //Add 1 highest digit
+        if (carry)
+            result.push_back('1');
             
-       string result = "";
-       int indexA = a.length() - 1;
-       int indexB = b.length() - 1;
-       
-       int carry = 0;
-       int sum = 0;
-       while (indexA >= 0 || indexB >= 0) {
-		   sum = carry;
-		   
-		   //Add digit by digit
-		   sum += indexA >= 0 ? a[indexA--] - '0' : 0;
-		   sum += indexB >= 0 ? b[indexB--] - '0' : 0;
-		   
-		   char digit = (sum & 0x01) + '0';
-		   result = digit + result;
-		   carry = sum >> 1;
-       }
-       
-	   if (carry)
-		   result = '1' + result;
+        reverse(result);
+        
+        return result;
+    }
 
-       return result;
+private:
+    char addDigit(char c1, char c2, bool &carry) {
+        char ch;
+        
+        if (c1 == '0' && c2 == '0') {
+            ch = carry ? '1' : '0';
+            carry = false;
+        } else if (c1 == '0' && c2 == '1' || c1 == '1' && c2 == '0') {
+            //carry won't change if current sum is 1
+            ch = carry ? '0' : '1';
+        } else {
+            ch = carry ? '1' : '0';
+            carry = true;
+        }
+        
+        return ch;
+    }
+    
+    void reverse(string &s) {
+        int i = 0;
+        int j = s.size() - 1;
+        char tmp;
+        while (i < j) {
+            tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+            i++;
+            j--;
+        }
     }
 };

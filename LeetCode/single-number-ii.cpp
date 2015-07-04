c2e3001614707847b9ad1f15797b4211684ce556
bluesilence <bluesilencemail@126.com>
2015-07-04 08:41:12 +0800

    Update single-number-ii.cpp
    Another solution which is shorter

--- LeetCode/single-number-ii.cpp
+++ LeetCode/single-number-ii.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        //Count how many 1s each bit has after summing up the whole array
-        //count % 3 is the bit contributed by the exceptional number
-        const int BITS_LEN = 8 * sizeof(int);
-        vector<int> bitCounts(BITS_LEN, 0);
+        //Another solution from Discuss
         const int N = nums.size();
         
-        int result = 0;
+        int ones = 0, twos = 0;
         
-        for (int i = 0; i < BITS_LEN; i++) {
-            for (int j = 0; j < N; j++) {
-                bitCounts[i] += (nums[j] >> i) & 1;
-            }
-            
-            result |= (bitCounts[i] % 3) << i;
+        for(int i = 0; i < N; i++) {
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
         }
-        
-        return result;
+    
+        return ones;
     }
 };

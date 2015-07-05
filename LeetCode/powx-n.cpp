153301a1acaa98142b53bc9641b8c2d3386a72a6
bluesilence <bluesilencemail@126.com>
2015-07-05 11:52:55 +0800

    Update powx-n.cpp
    Second time.
    Note:
    1. Leetcode has added test case for integer overflow of n;
    2. Use a vector to store intermediate pow results will cause Runtime error when absolute n is large, and is unnecessary. Because for a given N, only logN intermediate pows are calculated, and none of them is calculated more than once.

--- LeetCode/powx-n.cpp
+++ LeetCode/powx-n.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    double pow(double x, int n) {
-        if (0 == x)
+    double myPow(double x, int n) {
+        if (x == 0)
             return 0;
         
         if (n == 0)
             return 1;
-        else if (n < 0)
-            return 1 / powCore(x, 0 - n);
+            
+        double result = 0;
+        
+        if (n > 0)
+            result = powCore(x, n);
         else
-            return powCore(x, n);
+            result = 1 / powCore(x, 0 - (long long)n);
+        
+        return result;
     }
 
 private:
-    double powCore(double x, int n) {
-        if (0 == n)
+    double powCore(double x, long long n) {
+        if (x == 0)
+            return 0;
+        else if (n == 0)
             return 1;
             
-        double half = powCore(x, n/2);
+        double half = powCore(x, n >> 1);
         
-        if (0 == (n & 0x01)) {    //n is even
-            return half * half;
+        if (n & 0x01) {
+            return half * half * x;
         } else {
-            return x * half * half;
+            return half * half;
         }
     }
 };

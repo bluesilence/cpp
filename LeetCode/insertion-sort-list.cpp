78f45a2b9e22b55aed50cac95813e23fbc121075
bluesilence <bluesilencemail@126.com>
2015-07-05 13:35:16 +0800

    Update insertion-sort-list.cpp
    Second time.
    Note:
    1. L19: Split list;
    2. L21: When to end loop.

--- LeetCode/insertion-sort-list.cpp
+++ LeetCode/insertion-sort-list.cpp
@@ -6,49 +6,35 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
 class Solution {
 public:
-    ListNode *insertionSortList(ListNode *head) {
-        if (!head)
-            return NULL;
+    ListNode* insertionSortList(ListNode* head) {
+        if (!head || !head->next)
+            return head;
         
         ListNode *dummyHead = new ListNode(0);
         dummyHead->next = head;
         
-        //The ordered sequence is originally only 1 node: head
-        //Loop until the unordered part is NULL
-        //For each node A in the unordered part, find the first node B that has value > A in the ordered part
-        //If B is not null, insert A before B, else insert A at the tail of ordered part
-        //Modify the unordered part to start from original A's next node
-        ListNode *unOrdered = head->next;
+        ListNode *unordered = head->next;
+        head->next = NULL;  //Split ordered and unordered parts
         
-        //To avoid circular list
-        head->next = NULL;
-        
-        while (unOrdered) {
-            //Scan the entire ordered part to locate where to insert the unOrdered node
-			ListNode *tmpPre = dummyHead, *tmpOrdered = tmpPre->next;
-            ListNode *nextUnOrdered = unOrdered->next;
+        while (unordered) {    //There is still unordered node
+            ListNode *tmp = dummyHead;
             
-            while (tmpOrdered && tmpOrdered->val < unOrdered->val) {
-                tmpPre = tmpOrdered,
-                tmpOrdered = tmpOrdered->next;
+            while (tmp->next && tmp->next->val <= unordered->val) {
+                tmp = tmp->next;
             }
             
-            if (tmpOrdered != unOrdered) {  //Insert unOrdered between tmpPre and tmpOrdered
-                unOrdered->next = tmpOrdered;
-                tmpPre->next = unOrdered;
-            } else {    //Node unOrdered is greater than any nodes in the ordered part, no need to insert
-                unOrdered->next = NULL; //To avoid circular list
-            }
-            
-            unOrdered = nextUnOrdered;
+            ListNode *nextUnordered = unordered->next;
+            unordered->next = tmp->next;
+            tmp->next = unordered;
+            unordered = nextUnordered;
         }
         
-        ListNode *newHead = dummyHead->next;
+        head = dummyHead->next;
+        
         delete dummyHead;
         
-        return newHead;
+        return head;
     }
 };

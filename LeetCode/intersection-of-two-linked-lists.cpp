b99af7abf4ae5bd558f3cfc21761129787b54752
bluesilence <bluesilencemail@126.com>
2015-07-05 13:55:24 +0800

    Update intersection-of-two-linked-lists.cpp
    Second time.
    1 pass:)

--- LeetCode/intersection-of-two-linked-lists.cpp
+++ LeetCode/intersection-of-two-linked-lists.cpp
@@ -8,41 +8,35 @@
  */
 class Solution {
 public:
-    /**
-     * Best solution: If 2 lists have intersection, their last node must be the same!
-     */
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         if (!headA || !headB)
             return NULL;
         
-        ListNode *pA = headA, *pB = headB;
-        int lenA = getLen(headA), lenB = getLen(headB);
+        int lenA = getLen(headA);
+        int lenB = getLen(headB);
         
-        //Move the pointers to the same starting line
         if (lenA < lenB) {
             for (int i = lenA; i < lenB; i++) {
-                pB = pB->next;
+                headB = headB->next;
             }
-        } else if (lenB < lenA) {
+        } else if (lenA > lenB) {
             for (int i = lenB; i < lenA; i++) {
-                pA = pA->next;
+                headA = headA->next;
             }
         }
         
-        while (pA && pB && pA != pB) {
-            pA = pA->next;
-            pB = pB->next;
+        while (headA && headB && headA != headB) {
+            headA = headA->next;
+            headB = headB->next;
         }
         
-        if (pA == pB)
-            return pA;
-        else
-            return NULL;
+        return (headA == headB) ? headA : NULL;
     }
 
 private:
     int getLen(ListNode *head) {
         int len = 0;
+        
         while (head) {
             len++;
             head = head->next;

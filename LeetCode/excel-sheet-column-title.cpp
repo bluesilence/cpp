b661a6373bbb15c6076d08f9223a045d2732d90e
bluesilence <bluesilencemail@126.com>
2015-07-06 20:32:39 +0800

    Update excel-sheet-column-title.cpp
    Second time.
    Note:
    1. L7~8: when to use (n - 1) instead of n;
    2. L7, L11: Order of higher/lower digits.

--- LeetCode/excel-sheet-column-title.cpp
+++ LeetCode/excel-sheet-column-title.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     string convertToTitle(int n) {
-        if(n < 1) {
-            return "";
-        } else {
-            string result = "";
-            while(n) {  //get every letter in n from right to left
-                //This is very important to map to 0~25
-                n--;
-                result += n % alphabetCount + 'A';
-                n /= alphabetCount;
-            }
-            
-            reverse(result.begin(), result.end());
-            return result;
+        string result = "";
+        
+        while (n > 0) {
+            result += (n - 1) % 26 + 'A';   //Higher digits are added after lower digits
+            n = (n - 1) / 26;
         }
+        
+        reverse(result.begin(), result.end());
+        
+        return result;
     }
-
-private:
-    const int alphabetCount = 26;
 };

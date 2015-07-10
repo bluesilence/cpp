52a9f688ce873c1663efccb9054d6fe9f94dcb10
bluesilence <bluesilencemail@126.com>
2015-07-11 05:51:15 +0800

    Create lowest-common-ancestor-of-a-binary-search-tree.cpp
    First time.
    Recusion & List.
    Use vector to store the path instead of using queue.

---
+++ LeetCode/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || !p || !q)
+            return NULL;
+        
+        vector<TreeNode*> pathP;
+        vector<TreeNode*> pathQ;
+        
+        bool foundP = findPath(root, p, pathP);
+        bool foundQ = findPath(root, q, pathQ);
+        
+        TreeNode* lca = NULL;
+        
+        if (foundP && foundQ) {
+            int i = 0;
+            while (i < pathP.size() && i < pathQ.size()) {
+                if (pathP[i]->val != pathQ[i]->val) {
+                    break;
+                } else {
+                    lca = pathP[i];
+                    i++;
+                }
+            }
+        }
+        
+        return lca;
+    }
+
+private:
+    bool findPath(TreeNode* root, TreeNode* p, vector<TreeNode*> &path) {
+        if (!root)
+            return false;
+            
+        path.push_back(root);
+        
+        if (root->val != p->val) {
+            bool found = findPath(root->left, p, path);
+            if (!found) {
+                found = findPath(root->right, p, path);
+            }
+            
+            if (!found) {
+                path.pop_back();
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};

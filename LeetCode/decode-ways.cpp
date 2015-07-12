bfc9d0809fc0298bb81c749d0449883421060590
bluesilence <bluesilencemail@126.com>
2015-07-12 11:11:44 +0800

    Update decode-ways.cpp
    3rd time.
    L17~30:
    Differentiate '0' and non-'0' at index i.

--- LeetCode/decode-ways.cpp
+++ LeetCode/decode-ways.cpp
@@ -1,50 +1,46 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        if (s.empty())
+        //Define dp[N], dp[i] == the ways of decoding substring[0...i].
+        //dp[0] = 1
+        //dp[1] = dp[0] + (s[1] == '0' ? 0 : 1) + (isValid(substring[0...1]) ? 1 : 0)
+        const int N = s.size();
+        if (N == 0 || s[0] == '0')  //If the beginning is 0, there is no way to decode s
             return 0;
         
-        const int size = s.size();
-        //DP. Use an array ways[s.size()+1], ways[i] indicate how many ways to decode at s[i-1]
-        //This is to reduce duplicate code so that the loop can start from s[1] with ways[i-2]
-        //Special case: '0' can only be decoded with the digit prior to it
-        //ways[0] = 1
-        //if s[0] != '0', ways[1] = 1, else return 0
-        //if s[1] != '0', ways[2] = int(s[0...1]) <= 26 ? ways[0] + ways[1] : ways[1]
-        //Else, ways[1] = int(s[0...1]) <= 26 ? ways[0] : return 0
-        //So that ways[s.size()-1] is the total ways of decodings for s.
-        vector<int> ways(size+1, 0);
+        vector<int> dp(N, 0);
+        dp[0] = 1;
         
-        if (s[0] != '0') {
-            ways[0] = 1;
-            ways[1] = 1;
-        } else {
-            return 0;   //Invalid string at s[0]
-        }
-        
-        for (int i = 1; i < size; i++) {
-            if (s[i] != '0') {
-                ways[i+1] = ways[i];   //Decode as single digit
-                if (isValidTens(s[i-1], s[i])) {
-                    ways[i+1] += ways[i-1]; //Decode as double digits
-                }
+        for (int i = 1; i < N; i++) {
+            if (s[i] == '0') {
+                 //Invalid coding if current 0 cannot form a tens with previous character
+                 if (!isValid(s[i-1], s[i])) {
+                     return 0;
+                 } else {
+                     //Decode s[i-1, i] as 2 digits
+                     dp[i] = i >= 2 ? dp[i-2] : 1;
+                 }
             } else {
-                if (isValidTens(s[i-1], s[i])) {
-                    ways[i+1] = ways[i-1];  //Can only be decoded as double digits
-                } else {
-                    return 0;   //Invalid string at s[i]
-                }
+                 if (isValid(s[i-1], s[i])) {
+                     //Decode s[i-1, i] as 2 digits
+                     dp[i] = i >= 2 ? dp[i-2] : 1;
+                 }
+                 
+                 //Decode s[i] as 1 digit
+                 dp[i] += dp[i-1];
             }
         }
         
-        return ways[size];
+        return dp[N-1];
     }
 
 private:
-    bool isValidTens(char tenDigit, char oneDigit) {
-        if (tenDigit > '2' || tenDigit == '0' || tenDigit == '2' && oneDigit > '6')
+    bool isValid(char ten, char one) {
+        if (ten == '0') //'0' cannot be the ten, it must be the one of previous tens
             return false;
+            
+        int num = 10 * (ten - '0') + (one - '0');
         
-        return true;
+        return num <= 26 && num >= 10;
     }
 };

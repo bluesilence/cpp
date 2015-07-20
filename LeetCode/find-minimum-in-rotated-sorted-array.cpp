d03b2e1b8fb3a9513767d8114ca4ecf1cf7ec880
bluesilence <bluesilencemail@126.com>
2015-07-20 21:58:12 +0800

    Update find-minimum-in-rotated-sorted-array.cpp
    3rd time.
    Note:
    The return value is the min value, not the index.

--- LeetCode/find-minimum-in-rotated-sorted-array.cpp
+++ LeetCode/find-minimum-in-rotated-sorted-array.cpp
@@ -2,22 +2,31 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         if (nums.empty())
-            throw "EmptyArray";
+            throw "Input is empty";
         
+        if (nums.size() == 1)
+            return nums[0];
+            
         int begin = 0;
         int end = nums.size() - 1;
         int mid = 0;
         
-        while (begin < end) {   //NO "=" here, otherwise the loop cannot terminate because of L16
-            mid = (begin + end) / 2;
-            if (nums[mid] >= nums[begin] && nums[mid] > nums[end]) {  //mid falls in the left (greater) half; need to move right
-                begin = mid + 1;    //"+ 1" here, since minimum cannot be in the left half
-            } else {    //mid falls in the right (less) half; need to move left
-                end = mid;  //NO "- 1" here, since mid is within the less half and the minimal number should be included in the range
+        while (begin < end) {
+            if (nums[begin] < nums[end]) {  //Already sorted
+                return nums[begin];
+            }
+            
+            mid = begin + (end - begin) / 2;    //(begin + end) may overflow
+            
+            if (nums[mid] >= nums[begin]) { //Left part is sorted
+                begin = mid + 1;    //mid must be in the left part, while the min must exist in the right part
+            } else if (nums[mid] <= nums[end]) {    //Right part is sorted
+                end = mid;  //min must exist before or equal to mid
+            } else {
+                throw "Input is not a valid rotated array";
             }
         }
         
-        //Return begin/end both works, while mid doens't work, the reason is that mid is not updated after L12
         return nums[begin];
     }
 };

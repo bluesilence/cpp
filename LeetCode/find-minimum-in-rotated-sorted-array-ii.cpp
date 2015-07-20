70c5549cb53b623a8fbf6e630d60481504c9d388
bluesilence <bluesilencemail@126.com>
2015-07-20 22:17:00 +0800

    Update find-minimum-in-rotated-sorted-array-ii.cpp
    3rd time.
    The perfect solution:)

--- LeetCode/find-minimum-in-rotated-sorted-array-ii.cpp
+++ LeetCode/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
+        if (nums.empty())
+            throw "Input is empty";
+        
+        if (nums.size() == 1)
+            return nums[0];
+            
         int begin = 0;
         int end = nums.size() - 1;
-        int mid;
+        int mid = 0;
         
         while (begin < end) {
-            //The subsequence is already sorted
-            if (nums[begin] < nums[end])    //Necessary; otherwise when nums[begin] == nums[mid], L23 will skip the minimum number
+            if (nums[begin] < nums[end]) {  //Already sorted. Coverred cases when nums[mid] == nums[end] && nums[mid] > nums[begin]
                 return nums[begin];
-                
-            mid = (begin + end) / 2;
+            }
             
-            if (nums[mid] > nums[begin]) {
-                begin = mid + 1;
-            } else if (nums[mid] < nums[end]) {
-                end = mid;
-            } else {
+            mid = begin + (end - begin) / 2;    //(begin + end) may overflow
+            
+            if (nums[mid] == nums[begin] && nums[mid] == nums[end]) {   //No clue which part is sorted, move 1 step forward
                 begin++;
+            } else if (nums[mid] == nums[begin] && nums[mid] > nums[end] || nums[mid] > nums[begin]) { //Left part is sorted
+                begin = mid + 1;    //mid must be in the left part, while the min must exist in the right part
+            } else if (nums[mid] == nums[end] && nums[mid] < nums[begin] || nums[mid] < nums[end]) {    //Right part is sorted
+                end = mid;  //min must exist before or equal to mid
             }
         }
         

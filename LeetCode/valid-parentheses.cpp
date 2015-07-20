99dd862a6aec7127c656186da0a7e98a2eb36f49
bluesilence <bluesilencemail@126.com>
2015-07-20 22:31:17 +0800

    Update valid-parentheses.cpp
    3rd time.
    No need to use counters.
    1 stack suffices.

--- LeetCode/valid-parentheses.cpp
+++ LeetCode/valid-parentheses.cpp
@@ -1,61 +1,60 @@
 class Solution {
 public:
     bool isValid(string s) {
-        //For right parentheses, the other 2 kind of left parentheses must be all matched
-        //And the left parentheses of the same kind must have some left not matched yet
-        //Otherwise, the string is invalid
-        vector<int> counts(6, 0);
-        stack<char> lastParen;
+        //Use 1 stack to track latest parentheses
+        if (s.empty())
+            return true;
         
-        for (int i = 0; i < s.size(); i++) {
+        stack<char> parenStack;
+        
+        const int N = s.size();
+        
+        for (int i = 0; i < N; i++) {
             switch (s[i]) {
-                case '(':
-                    counts[0]++;
-                    lastParen.push('(');
+                case '(': {
+                    parenStack.push('(');
                     break;
-                case '[':
-                    counts[1]++;
-                    lastParen.push('[');
+                }
+                
+                case '{': {
+                    parenStack.push('{');
                     break;
-                case '{':
-                    counts[2]++;
-                    lastParen.push('{');
+                }
+                
+                case '[': {
+                    parenStack.push('[');
                     break;
-                case ')':   //Bug: && is prior to >, need to use ()
-                    if ((counts[0] > counts[3]) && lastParen.top() == '(') {
-                        counts[3]++;
-                        lastParen.pop();
+                }
+                
+                case ')': {
+                    if (!parenStack.empty() && parenStack.top() == '(') {
+                        parenStack.pop();
+                        break;
                     } else {
                         return false;
                     }
-                    
-                    break;
-                case ']':
-                    if ((counts[1] > counts[4]) && lastParen.top() == '[') {
-                        counts[4]++;
-                        lastParen.pop();
+                }
+                
+                case '}': {
+                    if (!parenStack.empty() && parenStack.top() == '{') {
+                        parenStack.pop();
+                        break;
                     } else {
                         return false;
                     }
-                    
-                    break;
-                case '}':
-                    if ((counts[2] > counts[5]) && lastParen.top() == '{') {
-                        counts[5]++;
-                        lastParen.pop();
+                }
+                
+                case ']': {
+                    if (!parenStack.empty() && parenStack.top() == '[') {
+                        parenStack.pop();
+                        break;
                     } else {
                         return false;
                     }
-                    
-                    break;
+                }
             }
         }
         
-        for (int i = 0; i < 3; i++) {
-            if (counts[i] != counts[i+3])
-                return false;
-        }
-        
-        return true;
+        return parenStack.empty();
     }
 };

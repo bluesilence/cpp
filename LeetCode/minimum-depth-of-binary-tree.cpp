eaec8d369986acecdbf5829a9e56101a5cc23303
bluesilence <bluesilencemail@126.com>
2015-07-21 21:17:57 +0800

    Update minimum-depth-of-binary-tree.cpp
    3rd time.

--- LeetCode/minimum-depth-of-binary-tree.cpp
+++ LeetCode/minimum-depth-of-binary-tree.cpp
@@ -13,11 +13,13 @@ public:
         if (!root)
             return 0;
         
-        if (!root->left)
-            return minDepth(root->right) + 1;
-        else if (!root->right)
-            return minDepth(root->left) + 1;
-        else
+        if (root->left && root->right)
             return min(minDepth(root->left), minDepth(root->right)) + 1;
+        else if (root->left)
+            return minDepth(root->left) + 1;
+        else if (root->right)
+            return minDepth(root->right) + 1;
+        else
+            return 1;
     }
 };

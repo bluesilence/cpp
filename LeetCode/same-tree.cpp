82483728d2c19658899a0245fdf1ef5eeb8e223c
bluesilence <bluesilencemail@126.com>
2015-07-21 22:03:15 +0800

    Update same-tree.cpp
    3rd time.
    Iterative way.

--- LeetCode/same-tree.cpp
+++ LeetCode/same-tree.cpp
@@ -16,6 +16,40 @@ public:
         if (!p || !q)
             return false;
         
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right) && p->val == q->val;
+        TreeNode* iterP = p;
+        TreeNode* iterQ = q;
+        stack<TreeNode*> stackP;
+        stack<TreeNode*> stackQ;
+    
+        while (iterP && iterQ || !stackP.empty() && !stackQ.empty()) {
+            while (iterP && iterQ) {
+                if (iterP->val != iterQ->val) {
+                    return false;
+                }
+                
+                stackP.push(iterP);
+                stackQ.push(iterQ);
+                
+                iterP = iterP->left;
+                iterQ = iterQ->left;
+            }
+            
+            if (iterP != NULL || iterQ != NULL)
+                return false;
+            
+            if (!stackP.empty() && !stackQ.empty()) {
+                iterP = stackP.top();
+                stackP.pop();
+                
+                iterP = iterP->right;
+                
+                iterQ = stackQ.top();
+                stackQ.pop();
+                
+                iterQ = iterQ->right;
+            }
+        }
+        
+        return true;
     }
 };

3bbf623f2bd63b239b5f1216a8e04d50bd0f251b
bluesilence <bluesilencemail@126.com>
2015-07-23 22:42:55 +0800

    Update majority-element.cpp
    3rd time.

--- LeetCode/majority-element.cpp
+++ LeetCode/majority-element.cpp
@@ -3,25 +3,22 @@ public:
     int majorityElement(vector<int>& nums) {
         const int N = nums.size();
         
-        if (N < 1)
-            throw "Array size must be >= 1";
-        
-        int majority = nums[0];
         int count = 1;
+        int major = nums[0];
         
         for (int i = 1; i < N; i++) {
-            if (majority != nums[i]) {
-                if (!count) {
-                    majority = nums[i];
-                    count = 1;
-                } else {
-                    count--;
-                }
-            } else {
+            if (nums[i] == major) {
                 count++;
+            } else {
+                if (count > 0) {
+                    count--;
+                } else {
+                    major = nums[i];
+                    count = 1;
+                }
             }
         }
         
-        return majority;
+        return major;
     }
 };

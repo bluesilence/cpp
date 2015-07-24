c76f0ff0c3891d3fcc9a8bbf6e68b698d71ee2c1
bluesilence <bluesilencemail@126.com>
2015-07-24 22:26:18 +0800

    Update regular-expression-matching.cpp
    3rd time.
    DP solution.
    The only initialization boundary is dp[0][0].
    Empty s can also be judged in the for loops.

--- LeetCode/regular-expression-matching.cpp
+++ LeetCode/regular-expression-matching.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        if (s.empty() && p.empty())
-            return true;
+        const int SLEN = s.size();
+        const int PLEN = p.size();
         
-        return matchCore(s, 0, p, 0);
-    }
-
-private:
-    bool matchCore(string s, int sBegin, string p, int pBegin) {
-        if (sBegin == s.size() && pBegin == p.size())
-            return true;
+        //dp[i][j] stands for whether s[0...i-1] matches p[0...j-1]
+        vector<vector<bool>> dp(SLEN + 1, vector<bool>(PLEN + 1, false));
         
-        char pChar = p[pBegin];
-        bool result = false;
-        if (pBegin + 1 < p.size()) {
-            char pCharNext = p[pBegin + 1];
+        dp[0][0] = true;
         
-            if (pCharNext == '*') {
-                result = matchCore(s, sBegin, p, pBegin + 2);
-                while (!result && sBegin < s.size() && (s[sBegin] == pChar || pChar == '.')) {
-                    sBegin++;
-                    result = matchCore(s, sBegin, p, pBegin + 2);
+        for (int i = 0; i <= SLEN; i++) {
+            for (int j = 1; j <= PLEN; j++) {
+                if (p[j-1] != '*') {
+                    dp[i][j] = i > 0 && dp[i-1][j-1] && (p[j-1] == '.' || s[i-1] == p[j-1]);
+                } else {    //Assume the pattern is always valid so there is at least 1 char before '*', i.e. j >= 2
+                    dp[i][j] = dp[i][j-2] || (i > 0 && dp[i-1][j] && (p[j-2] == '.' || s[i-1] == p[j-2]));
                 }
             }
         }
         
-        if (!result) {
-            result = (s[sBegin] == p[pBegin] || p[pBegin] == '.') && matchCore(s, sBegin + 1, p, pBegin + 1);
-        }
-        
-        return result;
+        return dp[SLEN][PLEN];
     }
 };

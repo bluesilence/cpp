350abd4da30e15c2c3fa6d0d813d05fa81ecfeec
bluesilence <bluesilencemail@126.com>
2015-07-25 11:21:01 +0800

    Update spiral-matrix.cpp
    3rd time.
    L40, L59: Correct comments.

--- LeetCode/spiral-matrix.cpp
+++ LeetCode/spiral-matrix.cpp
@@ -15,7 +15,7 @@ public:
 			}
 		} else {
             int minWidth = matrix.size() > matrix[0].size() ? matrix[0].size() : matrix.size();
-			int round = minWidth & 0x01 == 0 ? minWidth >> 1 : 1 + minWidth >> 1;
+	    int round = minWidth & 0x01 == 0 ? minWidth >> 1 : 1 + minWidth >> 1;
             for(int i = 1; i <= round; i++) {
                 scanLeftToRight(matrix, result, i);
                 scanTopToBottom(matrix, result, i);
@@ -31,17 +31,17 @@ private:
     void scanLeftToRight(vector<vector<int> > &matrix, vector<int> &result, int round) {
         int startCol = round - 1;
         int endCol = matrix[0].size() - round;
-        for(int i = startCol; i <= endCol; i++ ) {
+        for (int i = startCol; i <= endCol; i++) {
             result.push_back(matrix[round - 1][i]);
         }
     }
     
     void scanRightToLeft(vector<vector<int> > &matrix, vector<int> &result, int round) {
-        //There are more cols in currrent round to traverse, aka. matrix.size() - 2 * (round - 1) > 1
-		if (round - 1 < matrix.size() - round) {
+        //There are more than 1 row in currrent round to traverse, aka. 2 * (round - 1) + 1 < matrix.size()
+		if (2 * (round - 1) + 1 < matrix.size()) {
 			int startCol = matrix[0].size() - round - 1;
 			int endCol = round - 1;
-			for(int i = startCol; i >= endCol; i-- ) {
+			for (int i = startCol; i >= endCol; i--) {
 				result.push_back(matrix[matrix.size() - round][i]);
 			}
 		}
@@ -50,17 +50,17 @@ private:
     void scanTopToBottom(vector<vector<int> > &matrix, vector<int> &result, int round) {
         int startRow = round;
         int endRow = matrix.size() - round;
-        for(int i = startRow; i <= endRow; i++ ) {
+        for (int i = startRow; i <= endRow; i++) {
             result.push_back(matrix[i][matrix[0].size() - round]);
         }
     }
     
     void scanBottomToTop(vector<vector<int> > &matrix, vector<int> &result, int round) {
-        //There are more rows in currrent round to traverse, aka. matrix[0].size() - 2 * (round - 1) > 1
-		if (round - 1 < matrix[0].size() - round) {
+        //There are more than 1 col in currrent round to traverse, i.e. 2 * (round - 1) + 1 < matrix[0].size()
+		if (2 * (round - 1) + 1 < matrix[0].size()) {
 			int startRow = matrix.size() - round - 1;
 			int endRow = round;
-			for(int i = startRow; i >= endRow; i-- ) {
+			for (int i = startRow; i >= endRow; i--) {
 				result.push_back(matrix[i][round - 1]);
 			}
 		}

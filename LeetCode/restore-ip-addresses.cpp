38f0503dfc8585120d9ccb224f70e718a3af4fb6
bluesilence <bluesilencemail@126.com>
2015-07-26 20:21:01 +0800

    Update restore-ip-addresses.cpp
    3rd time.
    Note:
    1. L19: begin + unitLen <=, not <
    2. L16: &&, not ||

--- LeetCode/restore-ip-addresses.cpp
+++ LeetCode/restore-ip-addresses.cpp
@@ -6,44 +6,52 @@ public:
         if (s.size() < 4)
             return results;
         
-        restoreIpCore(results, "", s, 4);
+        restoreIpCore(results, s, 0, "", 4);
         
         return results;
     }
 
 private:
-    void restoreIpCore(vector<string> &results, string result, string s, int n) {
-        if (s.size() == 0 && n == 0) {
-            results.push_back(result);
-            return;
-        }
-        
-        for (int i = 1; i <= 3 && i <= s.size(); i++) {  //The digits of this ip number
-            if (i + 3 * (n-1) < s.size())   //pruning for next recursion
-                continue;
-            
-            string ipStr = s.substr(0, i);
-            if (isValidIpStr(ipStr)) {
-                string ipStrWithDot = n > 1 ? ipStr + "." : ipStr;
-                restoreIpCore(results, result + ipStrWithDot, s.substr(i, s.size() - i), n - 1);
+    void restoreIpCore(vector<string>& results, string &s, int begin, string ipStr, int unitsLeft) {
+        if (begin == s.size() && unitsLeft == 0) {
+            results.push_back(ipStr);
+        } else {
+            for (int unitLen = 1; unitLen < 4 && begin + unitLen <= s.size(); unitLen++) {
+                if (begin + unitLen + 3 * (unitsLeft - 1) < s.size()) {
+                    continue;
+                }
+                
+                string unit = s.substr(begin, unitLen);
+                if (isValidIPUnit(unit)) {
+                    if (unitsLeft > 1) {
+                        unit += '.';
+                    }
+                    
+                    restoreIpCore(results, s, begin + unitLen, ipStr + unit, unitsLeft - 1);
+                }
             }
         }
     }
-    
-    bool isValidIpStr(string ipStr) {
-        int ip = 0;
-        
-        if (ipStr[0] == '0' && ipStr.size() > 1)    //0 can only appear alone if it's the first number in the ipStr
+
+    bool isValidIPUnit(string &unit) {
+        if (unit.empty() || unit.size() > 3)
             return false;
+        
+        if (unit[0] == '0' && unit.size() > 1)
+            return false;
+        
+        int ipNum = 0;
+        for (int i = 0; i < unit.size(); i++) {
+            if (unit[i] < '0' || unit[i] > '9') {
+                return false;
+            }
             
-        for (int i = 0; i < ipStr.size(); i++) {
-            if (ipStr[i] > '9' || ipStr[i] < '0')
+            ipNum *= 10;
+            ipNum += unit[i] - '0';
+            
+            if (ipNum > 255) {
                 return false;
-                
-            ip *= 10;
-            ip += ipStr[i] - '0';
-            if (ip > 255)
-                return false;
+            }
         }
         
         return true;
